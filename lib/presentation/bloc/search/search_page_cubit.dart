5bfcb3795c4a333f7f556a07788f2c526aff8d5e
Alexander Aks'onov <m.mirvua@gmail.com>
2026-01-29 14:48:59 +0200

    now search results are updated in real time;

--- lib/presentation/bloc/search/search_page_cubit.dart
+++ lib/presentation/bloc/search/search_page_cubit.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart' show Cubit;
 import 'package:test_futter_project/common/enums/car_type.dart';
+import 'package:test_futter_project/domain/entities/car_entity.dart';
 import 'package:test_futter_project/domain/repositories/car_repository.dart';
 import 'package:test_futter_project/presentation/bloc/search/search_page_state.dart';
 
@@ -7,19 +10,31 @@ class SearchPageCubit extends Cubit<SearchPageState> {
   SearchPageCubit(this._carRepository) : super(SearchPageState());
 
   final CarRepository _carRepository;
+  StreamSubscription? _carSubscription;
 
   void loadData() {
     emit(state.copyWith(isLoading: true));
 
-    final results = _carRepository
-        .getAllCars()
-        .where((car) => car.type == state.currentSelectedType.name)
-        .toList();
+    final results = filterCarsByType(_carRepository.getAllCars());
 
     emit(state.copyWith(results: results, isLoading: false));
+
+    _carSubscription = _carRepository.watchCars()?.listen((entities) {
+      emit(state.copyWith(results: filterCarsByType(entities)));
+    });
   }
 
   void updateTypeSelection(CarType newType) {
     emit(state.copyWith(currentSelectedType: newType));
+  }
+
+  List<CarEntity> filterCarsByType(List<CarEntity> list) {
+    return list.where((car) => car.type == state.currentSelectedType.name).toList();
+  }
+
+  @override
+  Future<void> close() {
+    _carSubscription?.cancel();
+    return super.close();
   }
 }
