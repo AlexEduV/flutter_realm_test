d8936c70ce4ed04b9f5cffb2146cfc8fe4aa024d
Alexander Aks'onov <m.mirvua@gmail.com>
2026-01-30 10:27:38 +0200

    working on the parameter filter drawer;

--- lib/presentation/pages/search/search_page.dart
+++ lib/presentation/pages/search/search_page.dart
@@ -133,7 +133,7 @@ class _SearchPageState extends State<SearchPage> {
           ),
           endDrawer: state.drawerOpened == SearchDrawerType.model
               ? ModelFilterDrawer(models: state.allModels)
-              : ParameterFilterDrawer(models: state.allModels),
+              : ParameterFilterDrawer(),
         );
       },
     );
--- lib/presentation/pages/search/widgets/parameter_filter_drawer.dart
+++ lib/presentation/pages/search/widgets/parameter_filter_drawer.dart
@@ -6,65 +6,25 @@ import 'package:test_futter_project/presentation/bloc/search/search_page_state.d
 import 'package:test_futter_project/utils/l10n.dart';
 
 class ParameterFilterDrawer extends StatelessWidget {
-  final List<String> models;
-
-  const ParameterFilterDrawer({required this.models, super.key});
+  const ParameterFilterDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SearchPageCubit, SearchPageState>(
       builder: (context, state) {
-        final selectedSet = Set<String>.from(state.selectedModels);
-        final allSet = Set<String>.from(models);
-        final areAllSelected =
-            selectedSet.length == allSet.length && selectedSet.containsAll(allSet);
-
         return Drawer(
           child: ListView(
             children: [
               DrawerHeader(
                 child: Text(
-                  AppLocalisations.searchFilterModelTitle,
+                  AppLocalisations.searchFilterParametersTitle,
                   style: AppTextStyles.zonaPro20,
                 ),
               ),
-
-              CheckboxListTile(
-                value: areAllSelected,
-                onChanged: (bool? value) {
-                  if (value ?? false) {
-                    context.read<SearchPageCubit>().updateModelSelection(models);
-                  } else {
-                    context.read<SearchPageCubit>().updateModelSelection([]);
-                  }
-                },
-                title: Text(AppLocalisations.searchFilterModelPlaceholder),
-                controlAffinity: ListTileControlAffinity.leading,
-              ),
-
-              ...models.map((model) {
-                final isSelected = selectedSet.contains(model);
-                return CheckboxListTile(
-                  value: isSelected,
-                  onChanged: (bool? newValue) {
-                    if (newValue == true) {
-                      context.read<SearchPageCubit>().addCarModelToSelection(model);
-                    } else {
-                      context.read<SearchPageCubit>().removeCarModelFromSelection(model);
-                    }
-                  },
-                  title: Text(model),
-                  controlAffinity: ListTileControlAffinity.leading,
-                );
-              }),
             ],
           ),
         );
       },
     );
-  }
-
-  bool areTwoListsEqual(List listA, List listB) {
-    return Set<String>.from(listA).containsAll(listB) && Set<String>.from(listB).containsAll(listA);
   }
 }
