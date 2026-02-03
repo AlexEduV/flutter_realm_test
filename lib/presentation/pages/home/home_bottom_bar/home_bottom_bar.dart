f96224eb320f262b78f7c3565ba5412a2d8f0025
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-03 11:09:37 +0200

    tree optimisation;

--- lib/presentation/pages/home/home_bottom_bar/home_bottom_bar.dart
+++ lib/presentation/pages/home/home_bottom_bar/home_bottom_bar.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:test_futter_project/presentation/bloc/home/home_bottom_bar/home_bottom_bar_cubit.dart';
-import 'package:test_futter_project/presentation/bloc/home/home_bottom_bar/home_bottom_bar_state.dart';
 import 'package:test_futter_project/presentation/pages/home/home_bottom_bar/widgets/home_bottom_bar_item.dart';
 
 import '../../../../common/app_dimensions.dart';
@@ -14,45 +11,43 @@ class HomeBottomBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<HomeBottomBarCubit, HomeBottomBarState>(
-      builder: (context, state) {
-        return BottomAppBar(
-          padding: EdgeInsets.zero,
-          child: Material(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(AppDimensions.normalL),
-                topRight: Radius.circular(AppDimensions.normalL),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(AppDimensions.normalXS),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //todo: add an indexed enum or class for bar items
-                  const HomeBottomBarItem(index: 0, icon: Icons.home_outlined),
-                  const HomeBottomBarItem(index: 1, icon: Icons.favorite_border),
+    //todo: add shadow to the top
 
-                  IconButton(
-                    style: ButtonStyle(
-                      backgroundColor: const WidgetStatePropertyAll(Colors.black),
-                      foregroundColor: const WidgetStatePropertyAll(Colors.white),
-                      overlayColor: WidgetStatePropertyAll(Colors.white.withAlpha(60)),
-                    ),
-                    icon: const Icon(Icons.add, size: AppDimensions.bottomAppBarIconEnlargedSize),
-                    onPressed: onAddPressed,
-                    tooltip: AppLocalisations.addCarButtonTooltip,
-                  ),
-
-                  const HomeBottomBarItem(index: 2, icon: Icons.mail_outlined),
-                  const HomeBottomBarItem(index: 3, icon: Icons.settings_outlined),
-                ],
-              ),
-            ),
+    return BottomAppBar(
+      padding: EdgeInsets.zero,
+      child: Material(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(AppDimensions.normalL),
+            topRight: Radius.circular(AppDimensions.normalL),
           ),
-        );
-      },
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(AppDimensions.normalXS),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              //todo: add an indexed enum or class for bar items
+              const HomeBottomBarItem(index: 0, icon: Icons.home_outlined),
+              const HomeBottomBarItem(index: 1, icon: Icons.favorite_border),
+
+              IconButton(
+                style: ButtonStyle(
+                  backgroundColor: const WidgetStatePropertyAll(Colors.black),
+                  foregroundColor: const WidgetStatePropertyAll(Colors.white),
+                  overlayColor: WidgetStatePropertyAll(Colors.white.withAlpha(60)),
+                ),
+                icon: const Icon(Icons.add, size: AppDimensions.bottomAppBarIconEnlargedSize),
+                onPressed: onAddPressed,
+                tooltip: AppLocalisations.addCarButtonTooltip,
+              ),
+
+              const HomeBottomBarItem(index: 2, icon: Icons.mail_outlined),
+              const HomeBottomBarItem(index: 3, icon: Icons.settings_outlined),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
