0fec3c6b8be28d1f265ee3dca0c7808477747a04
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-03 16:38:52 +0200

    organised assets;

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ void main() async {
   await initDependenciesContainer();
 
   AppLocalisations.localisations = await LocalisationUtil.loadLocalisations(
-    'assets/mocks/localisation_mock_response_data_it.json',
+    'assets/mocks/it/localisation_mock_response_data.json',
   );
 
   await LocalisationUtil.saveLocalisations(AppLocalisations.localisations);
