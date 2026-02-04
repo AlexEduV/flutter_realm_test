f017e9426b45365660f297541e7bd5f66ec36bbd
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-04 11:29:05 +0200

    more tests;

---
+++ test/presentation/pages/search/widgets/model_filter_drawer_test.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test_futter_project/common/app_text_styles.dart';
+import 'package:test_futter_project/presentation/bloc/search/search_page_cubit.dart';
+import 'package:test_futter_project/presentation/bloc/search/search_page_state.dart';
+import 'package:test_futter_project/presentation/pages/search/widgets/model_filter_drawer.dart';
+import 'package:test_futter_project/utils/l10n.dart';
+
+@GenerateMocks([SearchPageCubit])
+void main() {
+  setUp(() {
+    AppLocalisations.localisations = {
+      'filters.model.title': 'Model',
+      'filters.model.placeholder': 'All',
+    };
+  });
+
+  group('ModelFilterDrawer', () {
+    late MockSearchPageCubit mockCubit;
+    late SearchPageState initialState;
+    final models = ['A', 'B', 'C'];
+
+    setUp(() {
+      mockCubit = MockSearchPageCubit();
+      initialState = SearchPageState(selectedModels: []);
+    });
+
+    Widget buildTestWidget(SearchPageState state) {
+      return MaterialApp(
+        home: BlocProvider<SearchPageCubit>.value(
+          value: mockCubit,
+          child: ModelFilterDrawer(models: models),
+        ),
+      );
+    }
+
+    testWidgets('displays header and "All" checkbox', (WidgetTester tester) async {
+      when(mockCubit.state).thenReturn(initialState);
+      whenListen(
+        mockCubit,
+        Stream<SearchPageState>.fromIterable([initialState]),
+        initialState: initialState,
+      );
+
+      await tester.pumpWidget(buildTestWidget(initialState));
+      await tester.pumpAndSettle();
+
+      expect(find.text(AppLocalisations.searchFilterModelTitle), findsOneWidget);
+      expect(find.text(AppLocalisations.searchFilterModelPlaceholder), findsOneWidget);
+      expect(find.byType(CheckboxListTile), findsNWidgets(models.length + 1)); // "All" + each model
+    });
+
+    testWidgets('displays model checkboxes', (WidgetTester tester) async {
+      when(mockCubit.state).thenReturn(initialState);
+      whenListen(
+        mockCubit,
+        Stream<SearchPageState>.fromIterable([initialState]),
+        initialState: initialState,
+      );
+
+      await tester.pumpWidget(buildTestWidget(initialState));
+      await tester.pumpAndSettle();
+
+      for (final model in models) {
+        expect(find.text(model), findsOneWidget);
+      }
+    });
+
+    testWidgets('tapping "All" checkbox calls updateModelSelection', (WidgetTester tester) async {
+      when(mockCubit.state).thenReturn(initialState);
+      whenListen(
+        mockCubit,
+        Stream<SearchPageState>.fromIterable([initialState]),
+        initialState: initialState,
+      );
+
+      await tester.pumpWidget(buildTestWidget(initialState));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text(AppLocalisations.searchFilterModelPlaceholder));
+      await tester.pump();
+
+      verify(mockCubit.updateModelSelection(models)).called(1);
+    });
+
+    testWidgets('tapping a model checkbox calls addCarModelToSelection', (
+      WidgetTester tester,
+    ) async {
+      when(mockCubit.state).thenReturn(initialState);
+      whenListen(
+        mockCubit,
+        Stream<SearchPageState>.fromIterable([initialState]),
+        initialState: initialState,
+      );
+
+      await tester.pumpWidget(buildTestWidget(initialState));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('A'));
+      await tester.pump();
+
+      verify(mockCubit.addCarModelToSelection('A')).called(1);
+    });
+
+    testWidgets('unchecking a model checkbox calls removeCarModelFromSelection', (
+      WidgetTester tester,
+    ) async {
+      final selectedState = SearchPageState(selectedModels: ['A']);
+      when(mockCubit.state).thenReturn(selectedState);
+      whenListen(
+        mockCubit,
+        Stream<SearchPageState>.fromIterable([selectedState]),
+        initialState: selectedState,
+      );
+
+      await tester.pumpWidget(buildTestWidget(selectedState));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('A'));
+      await tester.pump();
+
+      verify(mockCubit.removeCarModelFromSelection('A')).called(1);
+    });
+  });
+}
