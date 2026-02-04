8c8b43b8d5c1c28b85fe85b1d4d73e9a7177fb24
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-04 12:02:56 +0200

    tried to add more tests;

---
+++ test/presentation/pages/home/home_bottom_bar/widgets/home_bottom_bar_item_test.dart
@@ -0,0 +1,92 @@
+/* import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test_futter_project/common/app_colors.dart';
+import 'package:test_futter_project/presentation/bloc/home/home_bottom_bar/home_bottom_bar_cubit.dart';
+import 'package:test_futter_project/presentation/bloc/home/home_bottom_bar/home_bottom_bar_state.dart';
+import 'package:test_futter_project/presentation/pages/home/home_bottom_bar/widgets/home_bottom_bar_item.dart';
+
+import 'home_bottom_bar_item_test.mocks.dart';
+
+@GenerateMocks([HomeBottomBarCubit])
+void main() {
+  group('HomeBottomBarItem', () {
+    late MockHomeBottomBarCubit mockCubit;
+    late HomeBottomBarState state;
+
+    setUp(() {
+      mockCubit = MockHomeBottomBarCubit();
+      state = const HomeBottomBarState(currentSelectedTabIndex: 1);
+    });
+
+    Widget buildTestWidget({required int index, required IconData icon}) {
+      return MaterialApp(
+        home: BlocProvider<HomeBottomBarCubit>.value(
+          value: mockCubit,
+          child: HomeBottomBarItem(index: index, icon: icon),
+        ),
+      );
+    }
+
+    testWidgets('displays the correct icon', (WidgetTester tester) async {
+      when(mockCubit.state).thenReturn(state);
+      whenListen(mockCubit, Stream<HomeBottomBarState>.fromIterable([state]), initialState: state);
+
+      await tester.pumpWidget(buildTestWidget(index: 1, icon: Icons.home));
+      expect(find.byIcon(Icons.home), findsOneWidget);
+    });
+
+    testWidgets('uses selected color when selected', (WidgetTester tester) async {
+      when(mockCubit.state).thenReturn(const HomeBottomBarState(currentSelectedTabIndex: 2));
+      whenListen(
+        mockCubit,
+        Stream<HomeBottomBarState>.fromIterable([
+          const HomeBottomBarState(currentSelectedTabIndex: 2),
+        ]),
+        initialState: const HomeBottomBarState(currentSelectedTabIndex: 2),
+      );
+
+      await tester.pumpWidget(buildTestWidget(index: 2, icon: Icons.search));
+      final iconButton = tester.widget<IconButton>(find.byType(IconButton));
+      final style = iconButton.style;
+      final foregroundColor = style?.foregroundColor?.resolve({});
+
+      expect(foregroundColor, AppColors.headerColor);
+    });
+
+    testWidgets('uses unselected color when not selected', (WidgetTester tester) async {
+      when(mockCubit.state).thenReturn(const HomeBottomBarState(currentSelectedTabIndex: 0));
+      whenListen(
+        mockCubit,
+        Stream<HomeBottomBarState>.fromIterable([
+          const HomeBottomBarState(currentSelectedTabIndex: 0),
+        ]),
+        initialState: const HomeBottomBarState(currentSelectedTabIndex: 0),
+      );
+
+      await tester.pumpWidget(buildTestWidget(index: 1, icon: Icons.search));
+      final iconButton = tester.widget<IconButton>(find.byType(IconButton));
+      final style = iconButton.style;
+      final foregroundColor = style?.foregroundColor?.resolve({});
+
+      expect(foregroundColor, AppColors.headerColor.withAlpha((0.38 * 255).toInt()));
+    });
+
+    testWidgets('calls updateSelectedIndex when pressed', (WidgetTester tester) async {
+      when(mockCubit.state).thenReturn(state);
+      whenListen(mockCubit, Stream<HomeBottomBarState>.fromIterable([state]), initialState: state);
+
+      await tester.pumpWidget(buildTestWidget(index: 1, icon: Icons.home));
+      await tester.tap(find.byType(IconButton));
+      await tester.pump();
+
+      verify(mockCubit.updateSelectedIndex(1)).called(1);
+    });
+  });
+}
+ */
+
+void main() {}
