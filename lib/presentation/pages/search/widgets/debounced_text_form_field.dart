1dd2d153288de2a7fa3ccb40da6f047809b697ba
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-05 18:01:50 +0200

    added debounced text form field;

---
+++ lib/presentation/pages/search/widgets/debounced_text_form_field.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../../../../common/app_colors.dart';
+import '../../../../common/app_dimensions.dart';
+
+class DebouncedTextFormField extends StatefulWidget {
+  final TextEditingController controller;
+  final String label;
+  final void Function(String value) onDebouncedChanged;
+
+  const DebouncedTextFormField({
+    required this.controller,
+    required this.label,
+    required this.onDebouncedChanged,
+    super.key,
+  });
+
+  @override
+  State<DebouncedTextFormField> createState() => _DebouncedTextFormFieldState();
+}
+
+class _DebouncedTextFormFieldState extends State<DebouncedTextFormField> {
+  Timer? _debounce;
+  final _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addListener(_onTextChanged);
+  }
+
+  void _onTextChanged() {
+    if (!_focusNode.hasFocus) return;
+
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(milliseconds: 1000), () {
+      // Unfocus the field
+      _focusNode.unfocus();
+      // Pass value to cubit
+      widget.onDebouncedChanged(widget.controller.text);
+    });
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(_onTextChanged);
+    _debounce?.cancel();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      focusNode: _focusNode,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: AppDimensions.minorS,
+          horizontal: AppDimensions.minorL,
+        ),
+        fillColor: Colors.white,
+        filled: true,
+        labelText: widget.label,
+        border: const OutlineInputBorder(borderSide: BorderSide(color: AppColors.accentColor)),
+      ),
+      keyboardType: TextInputType.number,
+    );
+  }
+}
