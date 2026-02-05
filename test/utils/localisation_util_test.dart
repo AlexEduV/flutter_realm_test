d0cf190ca8e346d3c76b321bfd368a6c82b54abc
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-05 09:36:59 +0200

    deleted unworkable test;

--- test/utils/localisation_util_test.dart
+++ test/utils/localisation_util_test.dart
@@ -1,58 +1,13 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:test_futter_project/utils/localisation_util.dart';
 
-@GenerateMocks([AssetBundle])
 void main() {
-  //late MockAssetBundle mockBundle;
-
   setUp(() {
-    //mockBundle = MockAssetBundle();
     SharedPreferences.setMockInitialValues({});
   });
 
   group('LocalisationUtil', () {
-    //todo: the class' static methods are not to be overriden
-    // test('loadLocalisations loads and flattens JSON', () async {
-    //   // Arrange
-    //   const jsonString = '''
-    //   {
-    //     "pages": {
-    //       "home": {
-    //         "title": "Home"
-    //       }
-    //     }
-    //   }
-    //   ''';
-    //   final expectedFlat = {'pages.home.title': 'Home'};
-    //
-    //   // Mock rootBundle.loadString
-    //   when(mockBundle.loadString('assets/localisations.json')).thenAnswer((_) async => jsonString);
-    //
-    //   // Patch rootBundle for this test
-    //   final originalRootBundle = rootBundle;
-    //   TestWidgetsFlutterBinding.ensureInitialized();
-    //   ServicesBinding.instance.defaultBinaryMessenger.setMockMessageHandler('flutter/assets', (
-    //     message,
-    //   ) async {
-    //     return null;
-    //   });
-    //
-    //   // Patch JsonUtil.flattenJson for this test
-    //   JsonUtil.flattenJson = (Map<String, dynamic> json, [String prefix = '']) => expectedFlat;
-    //
-    //   // Act
-    //   final result = await LocalisationUtil.loadLocalisations('assets/localisations.json');
-    //
-    //   // Assert
-    //   expect(result, expectedFlat);
-    //
-    //   // Restore
-    //   JsonUtil.flattenJson = JsonUtil.flattenJsonOriginal;
-    // });
-
     test('saveLocalisations saves all key-value pairs to SharedPreferences', () async {
       final localisations = {'key1': 'value1', 'key2': 'value2'};
 
