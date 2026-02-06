5988dcaa5d91329df10b75c06aae7c4f9ce58c7d
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-06 11:59:33 +0200

    added translations;

--- lib/utils/l10n.dart
+++ lib/utils/l10n.dart
@@ -36,45 +36,49 @@ class AppLocalisations {
 
   static final String parameterYearName = get('filters.parameters.year');
 
-  static final String parameterBodyTypeName = get('filters.parameters.bodyType');
+  static final String parameterBodyTypeName = get('filters.parameters.bodyTypes.title');
 
   static final String parameterPriceRangeName = get('filters.parameters.priceRange');
 
-  static final String parameterFuelTypeName = get('filters.parameters.fuelType');
+  static final String parameterFuelTypeName = get('filters.parameters.fuelTypes.title');
 
-  static final String parameterTransmissionTypeName = get('filters.parameters.transmissionType');
+  static final String parameterTransmissionTypeName = get(
+    'filters.parameters.transmissionTypes.title',
+  );
 
   static final String comingSoonPlaceholderPageTitle = get('pages.comingSoon.title');
 
   static final String comingSoonPlaceholderPageSubTitle = get('pages.comingSoon.subtitle');
 
-  static final String bodyTypeSedan = 'Sedan';
+  static final String bodyTypeSedan = get('filters.parameters.bodyTypes.sedan');
 
-  static final String bodyTypeHatchback = 'Hatchback';
+  static final String bodyTypeHatchback = get('filters.parameters.bodyTypes.hatchback');
 
-  static final String bodyTypeUniversal = 'Universal';
+  static final String bodyTypeUniversal = get('filters.parameters.bodyTypes.universal');
 
-  static final String bodyTypeMinivan = 'Minivan';
+  static final String bodyTypeMinivan = get('filters.parameters.bodyTypes.minivan');
 
-  static final String bodyTypeCoupe = 'Coupe';
+  static final String bodyTypeCoupe = get('filters.parameters.bodyTypes.coupe');
 
-  static final String bodyTypeCrossover = 'Crossover';
+  static final String bodyTypeCrossover = get('filters.parameters.bodyTypes.crossover');
 
-  static final String bodyTypeSemi = 'Semi';
+  static final String bodyTypeSemi = get('filters.parameters.bodyTypes.semi');
 
-  static final String bodyTypeBike = 'Bike';
+  static final String bodyTypeBike = get('filters.parameters.bodyTypes.bike');
 
-  static final String fuelTypeDiesel = 'Diesel';
+  static final String fuelTypeDiesel = get('filters.parameters.fuelTypes.diesel');
 
-  static final String fuelTypeGasoline = 'Gasoline';
+  static final String fuelTypeGasoline = get('filters.parameters.fuelTypes.gasoline');
 
-  static final String fuelTypeEv = 'EV';
+  static final String fuelTypeEv = get('filters.parameters.fuelTypes.ev');
 
-  static final String fuelTypeHybrid = 'Hybrid';
+  static final String fuelTypeHybrid = get('filters.parameters.fuelTypes.hybrid');
 
-  static final String transmissionTypeManual = 'Manual';
+  static final String transmissionTypeManual = get('filters.parameters.transmissionTypes.manual');
 
-  static final String transmissionTypeAutomatic = 'Automatic';
+  static final String transmissionTypeAutomatic = get(
+    'filters.parameters.transmissionTypes.automatic',
+  );
 
-  static final String transmissionTypeHybrid = 'Hybrid';
+  static final String transmissionTypeHybrid = get('filters.parameters.transmissionTypes.hybrid');
 }
