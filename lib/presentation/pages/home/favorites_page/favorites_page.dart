a32877f99637881ca82d6e263b176ea6b3e9755e
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-09 16:18:36 +0200

    working on the favorites page;

--- lib/presentation/pages/home/favorites_page/favorites_page.dart
+++ lib/presentation/pages/home/favorites_page/favorites_page.dart
@@ -14,73 +14,76 @@ class FavoritesPage extends StatelessWidget {
         title: const Text('Favorites', style: AppTextStyles.zonaPro30),
         backgroundColor: AppColors.scaffoldColor,
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Container(
-            margin: const EdgeInsets.only(
-              bottom: AppDimensions.normalL,
-              left: AppDimensions.normalL,
-              right: AppDimensions.normalL,
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(AppDimensions.normalXL),
-              color: Colors.white,
-            ),
-            child: Container(
-              padding: const EdgeInsets.all(AppDimensions.normalM),
-              child: Row(
-                children: [
-                  // Car Image
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Container(
-                      color: AppColors.placeholderColor,
-                      width: AppDimensions.favoriteItemPictureSize,
-                      height: AppDimensions.favoriteItemPictureSize,
-                    ),
-                  ),
-                  const SizedBox(width: AppDimensions.normalM),
-                  // Car Details
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text('Honda Civic 2007', style: AppTextStyles.zonaPro18),
-                        const SizedBox(height: AppDimensions.minorL),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.directions_car,
-                              size: AppDimensions.normalM,
-                              color: Colors.grey[700],
-                            ),
-                            const SizedBox(width: AppDimensions.minorL),
-                            Text(
-                              'Sedan',
-                              style: AppTextStyles.zonaPro14.copyWith(color: Colors.grey[700]),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: AppDimensions.normalL),
-                        Text(
-                          '\$ 20 000',
-                          style: AppTextStyles.zonaPro16.copyWith(color: Colors.green),
-                        ),
-                      ],
-                    ),
-                  ),
-                  // Favorite Icon
-                  const SizedBox(
-                    width: AppDimensions.favoriteButtonSize,
-                    height: AppDimensions.favoriteButtonSize,
-                    child: Icon(Icons.favorite, color: AppColors.gold),
-                  ),
-                ],
+      body: Padding(
+        padding: const EdgeInsets.only(top: AppDimensions.normalL),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.only(
+                bottom: AppDimensions.normalL,
+                left: AppDimensions.normalL,
+                right: AppDimensions.normalL,
               ),
-            ),
-          );
-        },
-        itemCount: 3,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(AppDimensions.normalXL),
+                color: Colors.white,
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(AppDimensions.normalM),
+                child: Row(
+                  children: [
+                    // Car Image
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Container(
+                        color: AppColors.placeholderColor,
+                        width: AppDimensions.favoriteItemPictureSize,
+                        height: AppDimensions.favoriteItemPictureSize,
+                      ),
+                    ),
+                    const SizedBox(width: AppDimensions.normalM),
+                    // Car Details
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Honda Civic 2007', style: AppTextStyles.zonaPro18),
+                          const SizedBox(height: AppDimensions.minorL),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.directions_car,
+                                size: AppDimensions.normalM,
+                                color: Colors.grey[700],
+                              ),
+                              const SizedBox(width: AppDimensions.minorL),
+                              Text(
+                                'Sedan',
+                                style: AppTextStyles.zonaPro14.copyWith(color: Colors.grey[700]),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: AppDimensions.normalL),
+                          Text(
+                            '\$ 20 000',
+                            style: AppTextStyles.zonaPro16.copyWith(color: Colors.green),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Favorite Icon
+                    const SizedBox(
+                      width: AppDimensions.favoriteButtonSize,
+                      height: AppDimensions.favoriteButtonSize,
+                      child: Icon(Icons.favorite, color: AppColors.gold),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          itemCount: 3,
+        ),
       ),
     );
   }
