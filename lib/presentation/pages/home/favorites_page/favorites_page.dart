49f88d0279f912621a52adfdd573a5743da6dd38
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-09 16:10:56 +0200

    working on the favorites page;

--- lib/presentation/pages/home/favorites_page/favorites_page.dart
+++ lib/presentation/pages/home/favorites_page/favorites_page.dart
@@ -16,11 +16,62 @@ class FavoritesPage extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(AppDimensions.normalS),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Text('Honda Civic 2007'.toUpperCase(), style: AppTextStyles.zonaPro20)],
+          return Container(
+            margin: const EdgeInsets.only(
+              bottom: AppDimensions.normalL,
+              left: AppDimensions.normalL,
+              right: AppDimensions.normalL,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(AppDimensions.normalXL),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: const EdgeInsets.all(AppDimensions.normalM),
+              child: Row(
+                children: [
+                  // Car Image
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Container(color: AppColors.placeholderColor, width: 100, height: 100),
+                  ),
+                  const SizedBox(width: AppDimensions.normalM),
+                  // Car Details
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Civic', style: AppTextStyles.zonaPro18),
+                        const SizedBox(height: AppDimensions.minorM),
+                        Text('Honda', style: TextStyle(fontSize: 14, color: Colors.grey[600])),
+                        const SizedBox(height: AppDimensions.minorL),
+                        Row(
+                          children: [
+                            Icon(Icons.directions_car, size: 16, color: Colors.grey[700]),
+                            const SizedBox(width: AppDimensions.minorL),
+                            Text('Sedan', style: TextStyle(fontSize: 14, color: Colors.grey[700])),
+                          ],
+                        ),
+                        const SizedBox(height: AppDimensions.normalL),
+                        const Text(
+                          '20 000',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.green,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // Favorite Icon
+                  const Icon(
+                    Icons.favorite,
+                    color: Colors.redAccent,
+                    size: AppDimensions.favoriteButtonSize,
+                  ),
+                ],
+              ),
             ),
           );
         },
