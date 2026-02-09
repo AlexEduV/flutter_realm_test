c61c93156f0c2b04e145af4e3cf1d759c80c5eed
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-09 17:37:00 +0200

    fixed ui paddings when scrolling;

--- lib/presentation/pages/home/favorites_page/favorites_page.dart
+++ lib/presentation/pages/home/favorites_page/favorites_page.dart
@@ -19,95 +19,91 @@ class FavoritesPage extends StatelessWidget {
         title: Text(AppLocalisations.favoritesPageTitle, style: AppTextStyles.zonaPro30),
         backgroundColor: AppColors.scaffoldColor,
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: AppDimensions.normalL),
-        child: BlocBuilder<ExplorePageCubit, ExplorePageState>(
-          builder: (context, state) {
-            final allCars = state.cars;
+      body: BlocBuilder<ExplorePageCubit, ExplorePageState>(
+        builder: (context, state) {
+          final allCars = state.cars;
 
-            final favoriteIds = context.read<UserDataCubit>().state.favoriteIds;
-            final favoriteEntities = allCars
-                .where((entity) => favoriteIds.contains(entity.carId))
-                .toList();
+          final favoriteIds = context.read<UserDataCubit>().state.favoriteIds;
+          final favoriteEntities = allCars
+              .where((entity) => favoriteIds.contains(entity.carId))
+              .toList();
 
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                final car = favoriteEntities[index];
+          return ListView.builder(
+            padding: const EdgeInsets.only(top: AppDimensions.normalL),
+            itemBuilder: (context, index) {
+              final car = favoriteEntities[index];
 
-                return Container(
-                  margin: const EdgeInsets.only(
-                    bottom: AppDimensions.normalL,
-                    left: AppDimensions.normalL,
-                    right: AppDimensions.normalL,
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(AppDimensions.normalXL),
-                    color: Colors.white,
-                  ),
-                  child: Container(
-                    padding: const EdgeInsets.all(AppDimensions.normalM),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Car Image
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(AppDimensions.normalM),
-                          child: Container(
-                            color: AppColors.placeholderColor,
-                            width: AppDimensions.favoriteItemPictureSize,
-                            height: AppDimensions.favoriteItemPictureSize,
-                          ),
+              return Container(
+                margin: const EdgeInsets.only(
+                  bottom: AppDimensions.normalL,
+                  left: AppDimensions.normalL,
+                  right: AppDimensions.normalL,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(AppDimensions.normalXL),
+                  color: Colors.white,
+                ),
+                child: Container(
+                  padding: const EdgeInsets.all(AppDimensions.normalM),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Car Image
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(AppDimensions.normalM),
+                        child: Container(
+                          color: AppColors.placeholderColor,
+                          width: AppDimensions.favoriteItemPictureSize,
+                          height: AppDimensions.favoriteItemPictureSize,
                         ),
-                        const SizedBox(width: AppDimensions.normalM),
-                        // Car Details
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${car.manufacturer} ${car.model} ${car.year}',
-                                style: AppTextStyles.zonaPro18,
-                              ),
-                              const SizedBox(height: AppDimensions.minorS),
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.directions_car,
-                                    size: AppDimensions.normalM,
-                                    color: Colors.grey[700],
-                                  ),
-                                  const SizedBox(width: AppDimensions.minorL),
-                                  Text(
-                                    car.bodyType,
-                                    style: AppTextStyles.zonaPro14.copyWith(
-                                      color: Colors.grey[700],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: AppDimensions.normalL),
-                              Text(
-                                '\$ ${car.price}',
-                                style: AppTextStyles.zonaPro16.copyWith(color: Colors.green),
-                              ),
-                            ],
-                          ),
+                      ),
+                      const SizedBox(width: AppDimensions.normalM),
+                      // Car Details
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${car.manufacturer} ${car.model} ${car.year}',
+                              style: AppTextStyles.zonaPro18,
+                            ),
+                            const SizedBox(height: AppDimensions.minorS),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.directions_car,
+                                  size: AppDimensions.normalM,
+                                  color: Colors.grey[700],
+                                ),
+                                const SizedBox(width: AppDimensions.minorL),
+                                Text(
+                                  car.bodyType,
+                                  style: AppTextStyles.zonaPro14.copyWith(color: Colors.grey[700]),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: AppDimensions.normalL),
+                            Text(
+                              '\$ ${car.price}',
+                              style: AppTextStyles.zonaPro16.copyWith(color: Colors.green),
+                            ),
+                          ],
                         ),
-                        // Favorite Icon
-                        const SizedBox(
-                          width: AppDimensions.favoriteButtonSize,
-                          height: AppDimensions.favoriteButtonSize,
-                          child: Icon(Icons.favorite, color: AppColors.gold),
-                        ),
-                      ],
-                    ),
+                      ),
+                      // Favorite Icon
+                      const SizedBox(
+                        width: AppDimensions.favoriteButtonSize,
+                        height: AppDimensions.favoriteButtonSize,
+                        child: Icon(Icons.favorite, color: AppColors.gold),
+                      ),
+                    ],
                   ),
-                );
-              },
-              itemCount: favoriteEntities.length,
-            );
-          },
-        ),
+                ),
+              );
+            },
+            itemCount: favoriteEntities.length,
+          );
+        },
       ),
     );
   }
