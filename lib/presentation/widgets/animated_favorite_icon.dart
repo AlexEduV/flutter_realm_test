fe489094f234ac90aa2dde5766f1b3dd611d1f48
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-09 14:55:42 +0200

    working on favorite animation;

--- lib/presentation/widgets/animated_favorite_icon.dart
+++ lib/presentation/widgets/animated_favorite_icon.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../../common/app_colors.dart';
-import '../../common/app_dimensions.dart';
+import 'package:test_futter_project/common/app_colors.dart';
+import 'package:test_futter_project/common/app_dimensions.dart';
 
 class AnimatedFavoriteIcon extends StatelessWidget {
   final bool isFavorite;
@@ -13,10 +12,31 @@ class AnimatedFavoriteIcon extends StatelessWidget {
     final favoriteButtonSize = 38.0;
 
     return Container(
-      width: favoriteButtonSize,
       height: favoriteButtonSize,
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(AppDimensions.minorL)),
-      child: Icon(isFavorite ? Icons.favorite : Icons.favorite_border, color: AppColors.gold),
+      width: favoriteButtonSize,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(AppDimensions.minorL),
+      ),
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: isFavorite ? 300 : 0),
+        transitionBuilder: (child, animation) => ScaleTransition(scale: animation, child: child),
+        child: TweenAnimationBuilder<double>(
+          key: ValueKey(isFavorite),
+          tween: isFavorite
+              ? Tween<double>(begin: 1.0, end: 1.0)
+              : Tween<double>(begin: 1.0, end: 1.0),
+          duration: Duration(milliseconds: isFavorite ? 300 : 0),
+          curve: Curves.easeInOut,
+          builder: (context, scale, child) => Transform.scale(
+            scale: scale,
+            child: Icon(
+              isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: isFavorite ? AppColors.gold : Colors.grey,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
