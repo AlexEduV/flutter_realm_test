0118ea5f9d65a944e41b0cb10de06cad17709518
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-10 16:55:28 +0200

    added a back button;

--- lib/presentation/pages/details/details_page.dart
+++ lib/presentation/pages/details/details_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:test_futter_project/common/app_colors.dart';
 import 'package:test_futter_project/common/app_dimensions.dart';
 import 'package:test_futter_project/common/app_text_styles.dart';
@@ -32,108 +33,131 @@ class _DetailsPageState extends State<DetailsPage> {
         builder: (context, state) {
           final car = state.car;
 
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          return Stack(
             children: [
-              Container(
-                height: 280,
-                decoration: BoxDecoration(
-                  color: AppColors.placeholderColor,
-                  borderRadius: BorderRadius.circular(32.0),
-                ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(AppDimensions.normalM),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  spacing: AppDimensions.minorM,
-                  children: [
-                    Text(
-                      '${car?.manufacturer ?? ''} ${car?.model ?? ''} ${car?.year ?? ''}',
-                      style: AppTextStyles.zonaPro24.copyWith(fontWeight: FontWeight.w500),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 280,
+                    decoration: BoxDecoration(
+                      color: AppColors.placeholderColor,
+                      borderRadius: BorderRadius.circular(32.0),
                     ),
+                  ),
 
-                    Row(
+                  Padding(
+                    padding: const EdgeInsets.all(AppDimensions.normalM),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: AppDimensions.minorM,
                       children: [
                         Text(
-                          '\$ ${car?.price ?? ''}',
-                          style: AppTextStyles.zonaPro18.copyWith(fontWeight: FontWeight.w600),
+                          '${car?.manufacturer ?? ''} ${car?.model ?? ''} ${car?.year ?? ''}',
+                          style: AppTextStyles.zonaPro24.copyWith(fontWeight: FontWeight.w500),
                         ),
 
-                        if (car?.isHotPromotion ?? false) ...[
-                          const Icon(Icons.whatshot, size: 18, color: Colors.red),
-                        ],
+                        Row(
+                          children: [
+                            Text(
+                              '\$ ${car?.price ?? ''}',
+                              style: AppTextStyles.zonaPro18.copyWith(fontWeight: FontWeight.w600),
+                            ),
+
+                            if (car?.isHotPromotion ?? false) ...[
+                              const Icon(Icons.whatshot, size: 18, color: Colors.red),
+                            ],
+                          ],
+                        ),
+
+                        const SizedBox(height: AppDimensions.minorS),
+
+                        Container(
+                          padding: const EdgeInsets.all(AppDimensions.normalS),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(AppDimensions.normalS),
+                          ),
+                          child: Column(
+                            spacing: AppDimensions.normalM,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    'Vehicle Specifications',
+                                    style: AppTextStyles.zonaPro18.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      spacing: AppDimensions.normalS,
+                                      children: [
+                                        SpecificationItem(
+                                          title: 'Body',
+                                          subtitle: car?.bodyType ?? '',
+                                        ),
+
+                                        SpecificationItem(
+                                          title: 'Engine',
+                                          subtitle: car?.fuelType ?? '',
+                                        ),
+
+                                        SpecificationItem(
+                                          title: 'Transmission',
+                                          subtitle: car?.transmissionType ?? '',
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      spacing: AppDimensions.normalS,
+                                      children: [
+                                        SpecificationItem(
+                                          title: 'Mileage',
+                                          subtitle: car?.kilometers.toString() ?? '',
+                                        ),
+
+                                        SpecificationItem(title: 'Year', subtitle: car?.year ?? ''),
+
+                                        SpecificationItem(
+                                          title: 'Color',
+                                          subtitle: car?.color ?? '',
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
+                  ),
+                ],
+              ),
 
-                    const SizedBox(height: AppDimensions.minorS),
-
-                    Container(
-                      padding: const EdgeInsets.all(AppDimensions.normalS),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.circular(AppDimensions.normalS),
-                      ),
-                      child: Column(
-                        spacing: AppDimensions.normalM,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                'Vehicle Specifications',
-                                style: AppTextStyles.zonaPro18.copyWith(
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ],
-                          ),
-
-                          Row(
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  spacing: AppDimensions.normalS,
-                                  children: [
-                                    SpecificationItem(title: 'Body', subtitle: car?.bodyType ?? ''),
-
-                                    SpecificationItem(
-                                      title: 'Engine',
-                                      subtitle: car?.fuelType ?? '',
-                                    ),
-
-                                    SpecificationItem(
-                                      title: 'Transmission',
-                                      subtitle: car?.transmissionType ?? '',
-                                    ),
-                                  ],
-                                ),
-                              ),
-
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  spacing: AppDimensions.normalS,
-                                  children: [
-                                    SpecificationItem(
-                                      title: 'Mileage',
-                                      subtitle: car?.kilometers.toString() ?? '',
-                                    ),
-
-                                    SpecificationItem(title: 'Year', subtitle: car?.year ?? ''),
-
-                                    SpecificationItem(title: 'Color', subtitle: car?.color ?? ''),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+              Positioned(
+                top: 55,
+                left: 0,
+                child: IconButton(
+                  onPressed: () => context.pop(),
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    size: AppDimensions.appBarIconSize,
+                    color: AppColors.headerColor,
+                  ),
                 ),
               ),
             ],
