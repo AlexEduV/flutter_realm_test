3397ac27bc62e3c4739135a6fad3f99f76a96cee
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-11 09:23:04 +0200

    moved vehicle specs widget to a separate class;

--- lib/presentation/pages/details/details_page.dart
+++ lib/presentation/pages/details/details_page.dart
@@ -7,7 +7,7 @@ import 'package:test_futter_project/common/app_text_styles.dart';
 import 'package:test_futter_project/presentation/bloc/details/details_page_cubit.dart';
 import 'package:test_futter_project/presentation/bloc/details/details_page_state.dart';
 import 'package:test_futter_project/presentation/pages/details/widgets/owner_widget.dart';
-import 'package:test_futter_project/presentation/pages/details/widgets/specification_item.dart';
+import 'package:test_futter_project/presentation/pages/details/widgets/vehicle_specs_widget.dart';
 
 class DetailsPage extends StatefulWidget {
   final String carId;
@@ -86,76 +86,7 @@ class _DetailsPageState extends State<DetailsPage> {
 
                         const SizedBox(height: AppDimensions.minorS),
 
-                        Container(
-                          padding: const EdgeInsets.all(AppDimensions.normalS),
-                          decoration: BoxDecoration(
-                            color: Colors.grey[300],
-                            borderRadius: BorderRadius.circular(AppDimensions.normalS),
-                          ),
-                          child: Column(
-                            spacing: AppDimensions.normalM,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    'Vehicle Specifications',
-                                    style: AppTextStyles.zonaPro18.copyWith(
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                ],
-                              ),
-
-                              Row(
-                                children: [
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      spacing: AppDimensions.normalS,
-                                      children: [
-                                        SpecificationItem(
-                                          title: 'Body',
-                                          subtitle: car?.bodyType ?? '',
-                                        ),
-
-                                        SpecificationItem(
-                                          title: 'Engine',
-                                          subtitle: car?.fuelType ?? '',
-                                        ),
-
-                                        SpecificationItem(
-                                          title: 'Transmission',
-                                          subtitle: car?.transmissionType ?? '',
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      spacing: AppDimensions.normalS,
-                                      children: [
-                                        SpecificationItem(
-                                          title: 'Mileage',
-                                          subtitle: car?.kilometers.toString() ?? '',
-                                        ),
-
-                                        SpecificationItem(title: 'Year', subtitle: car?.year ?? ''),
-
-                                        SpecificationItem(
-                                          title: 'Color',
-                                          subtitle: car?.color ?? '',
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
+                        if (car != null) ...[VehicleSpecsWidget(car: car)],
 
                         const SizedBox(height: AppDimensions.minorL),
 
---
+++ lib/presentation/pages/details/widgets/vehicle_specs_widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:test_futter_project/domain/entities/car_entity.dart';
+import 'package:test_futter_project/presentation/pages/details/widgets/specification_item.dart';
+
+import '../../../../common/app_dimensions.dart';
+import '../../../../common/app_text_styles.dart';
+
+class VehicleSpecsWidget extends StatelessWidget {
+  final CarEntity car;
+
+  const VehicleSpecsWidget({required this.car, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(AppDimensions.normalS),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(AppDimensions.normalS),
+      ),
+      child: Column(
+        spacing: AppDimensions.normalM,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                'Vehicle Specifications',
+                style: AppTextStyles.zonaPro18.copyWith(fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: AppDimensions.normalS,
+                  children: [
+                    SpecificationItem(title: 'Body', subtitle: car.bodyType),
+
+                    SpecificationItem(title: 'Engine', subtitle: car.fuelType),
+
+                    SpecificationItem(title: 'Transmission', subtitle: car.transmissionType),
+                  ],
+                ),
+              ),
+
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: AppDimensions.normalS,
+                  children: [
+                    SpecificationItem(title: 'Mileage', subtitle: car.kilometers.toString()),
+
+                    SpecificationItem(title: 'Year', subtitle: car.year ?? ''),
+
+                    SpecificationItem(title: 'Color', subtitle: car.color ?? ''),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
