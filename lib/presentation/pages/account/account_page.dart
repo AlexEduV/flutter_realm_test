f111720f519247b108615225ffe0389c4fe557b4
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-12 15:02:35 +0200

    working on account page;

--- lib/presentation/pages/account/account_page.dart
+++ lib/presentation/pages/account/account_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_futter_project/common/app_colors.dart';
 import 'package:test_futter_project/presentation/bloc/user/user_data_cubit.dart';
 import 'package:test_futter_project/presentation/bloc/user/user_data_state.dart';
 import 'package:test_futter_project/presentation/pages/authentication/login_page.dart';
+
+import '../../../common/app_text_styles.dart';
 
 class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
@@ -15,7 +18,27 @@ class AccountPage extends StatelessWidget {
           return const LoginPage();
         }
 
-        return const Placeholder();
+        return Scaffold(
+          backgroundColor: AppColors.scaffoldColor,
+          appBar: AppBar(
+            title: const Text('My Account', style: AppTextStyles.zonaPro20),
+            centerTitle: true,
+          ),
+          body: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Center(child: CircleAvatar(radius: 50, backgroundColor: Colors.white)),
+              ),
+
+              ListTile(title: Text('Personal Details')),
+              ListTile(title: Text('Location')),
+              ListTile(title: Text('Clear History')),
+              ListTile(title: Text('Log out')),
+            ],
+          ),
+        );
       },
     );
   }
