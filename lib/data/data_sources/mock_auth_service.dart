ee53506bf72bd2bda50f1dce9e2cbe6400430e83
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-12 17:49:51 +0200

    fixed warnings;

--- lib/data/data_sources/mock_auth_service.dart
+++ lib/data/data_sources/mock_auth_service.dart
@@ -1,13 +1,11 @@
 import 'package:test_futter_project/domain/data_sources/auth_service.dart';
-import 'package:test_futter_project/domain/data_sources/base_local_storage.dart';
 import 'package:test_futter_project/domain/models/auth_result.dart';
 import 'package:test_futter_project/domain/repositories/auth_repository.dart';
 
 class MockAuthService implements AuthService {
-  final BaseLocalStorage _localStorage;
   final AuthRepository _authRepository;
 
-  MockAuthService(this._localStorage, this._authRepository);
+  MockAuthService(this._authRepository);
 
   @override
   Future<AuthResult> login(String email, String password) async {
