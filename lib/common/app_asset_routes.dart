d4e4a3183d334b98d43e9615b09d176f3cb2448b
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-19 12:32:15 +0200

    added files;

--- lib/common/app_asset_routes.dart
+++ lib/common/app_asset_routes.dart
@@ -3,4 +3,8 @@ class AppAssetRoutes {
   static const imagesFolder = 'images/';
 
   static const yellowCarLoginBackground = '$assetFolder${imagesFolder}car-yellow.jpg';
+
+  static const scaniaTruckBlackImage = '$assetFolder${imagesFolder}scania-black.jpg';
+  static const hondaCivicRedImage = '$assetFolder${imagesFolder}honda-civic-red.jpg';
+  static const porscheYellowImage = '$assetFolder${imagesFolder}porsche-yellow.jpg';
 }
