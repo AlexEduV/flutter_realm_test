f76521ec15cc8b07e811adaa5d0555026c552f55
Alexander Aks'onov <m.mirvua@gmail.com>
2026-02-27 10:40:10 +0200

    added a hover effect to the article item;

--- lib/presentation/pages/home/explore_page/widgets/explore_article_item.dart
+++ lib/presentation/pages/home/explore_page/widgets/explore_article_item.dart
@@ -6,32 +6,55 @@ import 'package:test_futter_project/presentation/widgets/app_semantics.dart';
 
 import '../../../../../common/app_colors.dart';
 
-class ExploreArticleItem extends StatelessWidget {
+class ExploreArticleItem extends StatefulWidget {
   final double height;
   final String articleName;
 
   const ExploreArticleItem({required this.articleName, this.height = 120.0, super.key});
 
   @override
+  State<ExploreArticleItem> createState() => _ExploreArticleItemState();
+}
+
+class _ExploreArticleItemState extends State<ExploreArticleItem> {
+  bool _pressed = false;
+
+  void _setPressed(bool value) {
+    setState(() {
+      _pressed = value;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AppSemantics(
-      label: AppSemanticsLabels.exploreArticleItem,
-      button: true,
-      child: Material(
-        color: AppColors.accentColor.withAlpha(60),
-        borderRadius: BorderRadius.circular(AppDimensions.normalL),
-        child: InkWell(
-          onTap: () {},
+    return AnimatedScale(
+      scale: _pressed ? 1.07 : 1.0, // Slightly enlarge when pressed
+      duration: const Duration(milliseconds: 120),
+      curve: Curves.easeOut,
+      child: AppSemantics(
+        label: AppSemanticsLabels.exploreArticleItem,
+        button: true,
+        child: Material(
+          color: AppColors.accentColor.withAlpha(60),
           borderRadius: BorderRadius.circular(AppDimensions.normalL),
-          child: Container(
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(AppDimensions.normalL)),
-            height: height,
-            width: 120,
-            child: Padding(
-              padding: const EdgeInsets.all(AppDimensions.minorL),
-              child: Align(
-                alignment: AlignmentGeometry.bottomLeft,
-                child: Text(articleName, maxLines: 2, style: AppTextStyles.zonaPro16White),
+          child: InkWell(
+            onTap: () {
+              // Your tap logic here
+            },
+            onTapDown: (_) => _setPressed(true),
+            onTapUp: (_) => _setPressed(false),
+            onTapCancel: () => _setPressed(false),
+            borderRadius: BorderRadius.circular(AppDimensions.normalL),
+            child: Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(AppDimensions.normalL)),
+              height: widget.height,
+              width: 120,
+              child: Padding(
+                padding: const EdgeInsets.all(AppDimensions.minorL),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(widget.articleName, maxLines: 2, style: AppTextStyles.zonaPro16White),
+                ),
               ),
             ),
           ),
