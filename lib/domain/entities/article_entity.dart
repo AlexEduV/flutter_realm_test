877a21b5e58b98a4c11dc914169f96b84a398f91
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-02 11:11:58 +0200

    enhanced article model;

--- lib/domain/entities/article_entity.dart
+++ lib/domain/entities/article_entity.dart
@@ -1,6 +1,9 @@
 class ArticleEntity {
   final String id;
   final String title;
+  final String authorFullName;
+  final String datePublished;
+  final int? minsToRead;
   final String summary;
   final List<String> paragraphs;
   final String? imageUrl;
@@ -10,11 +13,21 @@ class ArticleEntity {
     required this.title,
     required this.summary,
     required this.paragraphs,
+    required this.authorFullName,
+    required this.datePublished,
+    this.minsToRead,
     this.imageUrl,
   });
 
   factory ArticleEntity.empty() {
-    return const ArticleEntity(id: '', title: '', summary: '', paragraphs: []);
+    return const ArticleEntity(
+      id: '',
+      title: '',
+      summary: '',
+      paragraphs: [],
+      authorFullName: 'Anonymous',
+      datePublished: '20/11/2022',
+    );
   }
 
   static ArticleEntity fromJson(Map<String, dynamic> json) {
@@ -23,7 +36,10 @@ class ArticleEntity {
       title: json['title'] as String,
       summary: json['summary'] as String,
       paragraphs: List<String>.from(json['paragraphs'] as List),
-      imageUrl: json['imageUrl'],
+      imageUrl: json['imageUrl'] as String?,
+      minsToRead: json['mins_to_read'] as int?,
+      authorFullName: json['author'] ?? 'Anonymous',
+      datePublished: json['date_published'] as String,
     );
   }
 }
