7ed2d94c44b8c63bc90e945428c781ba832a180b
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-02 14:15:16 +0200

    added documentation;

--- lib/utils/image_cache_util.dart
+++ lib/utils/image_cache_util.dart
@@ -2,7 +2,8 @@ import 'package:flutter/cupertino.dart' show PaintingBinding;
 
 class ImageCacheUtil {
   static void initExtendedCacheSize() {
-    PaintingBinding.instance.imageCache.maximumSize = 1000;
-    PaintingBinding.instance.imageCache.maximumSizeBytes = 200 << 20; // 200 MB
+    PaintingBinding.instance.imageCache.maximumSize = 1000; // The default is a 100
+    PaintingBinding.instance.imageCache.maximumSizeBytes =
+        200 << 20; // 200 Mb, instead of a default 100 Mb
   }
 }
