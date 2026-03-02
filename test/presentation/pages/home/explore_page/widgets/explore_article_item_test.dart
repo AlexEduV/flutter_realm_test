94c3b0673bbab114d45d5f872e0e9d93c21f4176
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-02 16:43:25 +0200

    fixing tests;

--- test/presentation/pages/home/explore_page/widgets/explore_article_item_test.dart
+++ test/presentation/pages/home/explore_page/widgets/explore_article_item_test.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test_futter_project/common/app_colors.dart';
 import 'package:test_futter_project/common/app_dimensions.dart';
+import 'package:test_futter_project/di/injection_container.dart';
 import 'package:test_futter_project/domain/entities/article_entity.dart';
+import 'package:test_futter_project/presentation/bloc/home/explore_page/explore_page_cubit.dart';
+import 'package:test_futter_project/presentation/bloc/home/explore_page/explore_page_state.dart';
 import 'package:test_futter_project/presentation/pages/home/explore_page/widgets/explore_article_item.dart';
 
+import '../../../../../utils/app_router_test.mocks.dart';
+
 void main() {
+  final mockExplorePageCubit = MockExplorePageCubit();
+
+  setUpAll(() {
+    serviceLocator.registerSingleton<ExplorePageCubit>(mockExplorePageCubit);
+    when(
+      mockExplorePageCubit.state,
+    ).thenReturn(ExplorePageState(cars: [], articles: [ArticleEntity.empty()]));
+    when(mockExplorePageCubit.stream).thenAnswer((_) => const Stream.empty());
+  });
+
+  tearDownAll(() async {
+    await serviceLocator.reset();
+  });
+
+  Widget makeTestableWidget(Widget child) {
+    return MaterialApp(
+      home: MultiBlocProvider(
+        providers: [BlocProvider<ExplorePageCubit>.value(value: mockExplorePageCubit)],
+        child: child,
+      ),
+    );
+  }
+
   group('ExploreSectionItem', () {
     testWidgets('has default height and width', (WidgetTester tester) async {
       await tester.pumpWidget(
-        MaterialApp(
-          home: Scaffold(body: ExploreArticleItem(article: ArticleEntity.empty(), index: 0)),
+        makeTestableWidget(
+          Scaffold(body: ExploreArticleItem(article: ArticleEntity.empty(), index: 0)),
         ),
       );
 
@@ -22,9 +52,11 @@ void main() {
 
     testWidgets('uses custom height when provided', (WidgetTester tester) async {
       await tester.pumpWidget(
-        MaterialApp(
-          home: Scaffold(
-            body: ExploreArticleItem(height: 200.0, article: ArticleEntity.empty(), index: 0),
+        makeTestableWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: ExploreArticleItem(height: 200.0, article: ArticleEntity.empty(), index: 0),
+            ),
           ),
         ),
       );
@@ -37,8 +69,10 @@ void main() {
 
     testWidgets('has correct color and border radius', (WidgetTester tester) async {
       await tester.pumpWidget(
-        MaterialApp(
-          home: Scaffold(body: ExploreArticleItem(article: ArticleEntity.empty(), index: 0)),
+        makeTestableWidget(
+          MaterialApp(
+            home: Scaffold(body: ExploreArticleItem(article: ArticleEntity.empty(), index: 0)),
+          ),
         ),
       );
 
