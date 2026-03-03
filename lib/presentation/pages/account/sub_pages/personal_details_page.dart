d0a5427f874e036b543d12eb844376bb74a98f03
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-03 14:33:06 +0200

    working on the personal details page;

--- lib/presentation/pages/account/sub_pages/personal_details_page.dart
+++ lib/presentation/pages/account/sub_pages/personal_details_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:test_futter_project/common/app_colors.dart';
+import 'package:test_futter_project/common/app_dimensions.dart';
+import 'package:test_futter_project/utils/l10n.dart';
 
 import '../../../../common/app_text_styles.dart';
 
@@ -11,10 +13,16 @@ class PersonalDetailsPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.scaffoldColor,
       appBar: AppBar(
-        title: const Text('Personal Details', style: AppTextStyles.zonaPro20),
+        title: Text(AppLocalisations.accountItemPersonalDetails, style: AppTextStyles.zonaPro20),
         centerTitle: true,
       ),
-      body: const Column(children: [Text('First Name')]),
+      body: const Padding(
+        padding: EdgeInsets.all(AppDimensions.minorL),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [Text('First Name'), Text('Last Name'), Text('Email'), Text('Phone Number')],
+        ),
+      ),
     );
   }
 }
