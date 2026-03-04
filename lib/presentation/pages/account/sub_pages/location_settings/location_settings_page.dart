5300a3eb6ae1f14dc472febfbe5acab3b1c99a58
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-04 09:22:22 +0200

    working on the location page;

--- lib/presentation/pages/account/sub_pages/location_settings/location_settings_page.dart
+++ lib/presentation/pages/account/sub_pages/location_settings/location_settings_page.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../common/app_colors.dart';
+import '../../../../../common/app_dimensions.dart';
+import '../../../../../common/app_text_styles.dart';
+import '../../../../../domain/models/personal_details_item_model.dart';
+import '../../../../../utils/l10n.dart';
+import '../../../../bloc/user/user_data_cubit.dart';
+import '../../../../bloc/user/user_data_state.dart';
+import '../personal_details/widgets/personal_details_list_item.dart';
 
 class LocationSettingsPage extends StatelessWidget {
   const LocationSettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.scaffoldColor,
+      appBar: AppBar(
+        title: Text(AppLocalisations.accountItemLocation, style: AppTextStyles.zonaPro20),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(AppDimensions.minorL),
+        child: Container(
+          margin: const EdgeInsets.all(AppDimensions.minorL),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(AppDimensions.normalL),
+          ),
+          child: BlocBuilder<UserDataCubit, UserDataState>(
+            builder: (context, state) {
+              final items = [
+                PersonalDetailsItemModel(
+                  title: 'Location',
+                  subtitle: 'On',
+                  icon: Icons.location_on_outlined,
+                ),
+                PersonalDetailsItemModel(
+                  title: 'Current Country',
+                  subtitle: 'The UK',
+                  icon: Icons.public,
+                ),
+              ];
+
+              return ListView.separated(
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(AppDimensions.minorL),
+                itemCount: items.length,
+                separatorBuilder: (context, index) => const Divider(),
+                itemBuilder: (context, index) {
+                  final item = items[index];
+                  return PersonalDetailsListItem(
+                    title: item.title,
+                    description: item.subtitle,
+                    icon: item.icon,
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
