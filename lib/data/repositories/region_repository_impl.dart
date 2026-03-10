e4066e0bbdfe714414e198af21eb18f301becf10
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-10 10:28:48 +0200

    using constants where possible;

--- lib/data/repositories/region_repository_impl.dart
+++ lib/data/repositories/region_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
+import 'package:test_futter_project/common/app_asset_routes.dart';
 import 'package:test_futter_project/domain/repositories/region_repository.dart';
 
 import '../../domain/entities/region_entity.dart';
@@ -15,7 +16,9 @@ class RegionRepositoryImpl implements RegionRepository {
   @override
   Future<void> loadRegions() async {
     if (_regions != null) return; // Already loaded
-    final jsonString = await rootBundle.loadString('assets/mocks/regions_data.json');
+    final jsonString = await rootBundle.loadString(
+      '${AppAssetRoutes.assetFolder}${AppAssetRoutes.mocksFolder}regions_data.json',
+    );
     final List data = json.decode(jsonString);
     _regions = data.map((e) => RegionEntity.fromJson(e)).toList();
   }
