ec03ecfc9ba513b264749405974de8626e96f058
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-10 14:18:45 +0200

    added docs;

--- lib/common/extensions/context_extension.dart
+++ lib/common/extensions/context_extension.dart
@@ -4,7 +4,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../presentation/bloc/l10n/app_localisations_cubit.dart';
 
 extension L10nX on BuildContext {
+  //most use cases
   String tr(String key) => watch<AppLocalisationsCubit>().state.get(key);
 
+  //in the callbacks, listeners are not allowed
   String trRead(String key) => read<AppLocalisationsCubit>().state.get(key);
 }
