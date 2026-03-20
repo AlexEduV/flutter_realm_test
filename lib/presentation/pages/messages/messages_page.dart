96cba3a327b8a005130fb52ebea2631fda7ea45c
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-20 12:32:48 +0200

    moved chat message text field to the separate class;

--- lib/presentation/pages/messages/messages_page.dart
+++ lib/presentation/pages/messages/messages_page.dart
@@ -13,7 +13,7 @@ import 'package:test_futter_project/domain/usecases/users/get_user_by_id_use_cas
 import 'package:test_futter_project/presentation/bloc/home/inbox_page/inbox_page_cubit.dart';
 import 'package:test_futter_project/presentation/bloc/home/inbox_page/inbox_page_state.dart';
 import 'package:test_futter_project/presentation/bloc/messages/messages_page_cubit.dart';
-import 'package:test_futter_project/presentation/pages/messages/widgets/chat_input_bar.dart';
+import 'package:test_futter_project/presentation/pages/messages/widgets/chat_input_bar/chat_input_bar.dart';
 import 'package:test_futter_project/presentation/pages/messages/widgets/date_divider.dart';
 import 'package:test_futter_project/presentation/pages/messages/widgets/empty_conversation_placeholder.dart';
 import 'package:test_futter_project/presentation/pages/messages/widgets/message_item.dart';
---
+++ lib/presentation/pages/messages/widgets/chat_input_bar/chat_input_bar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_futter_project/common/app_semantics_labels.dart';
+import 'package:test_futter_project/common/enums/message_status.dart';
+import 'package:test_futter_project/domain/models/message_model.dart';
+import 'package:test_futter_project/presentation/bloc/home/inbox_page/inbox_page_cubit.dart';
+import 'package:test_futter_project/presentation/bloc/messages/messages_page_cubit.dart';
+import 'package:test_futter_project/presentation/bloc/messages/messages_page_state.dart';
+import 'package:test_futter_project/presentation/bloc/user/user_data_cubit.dart';
+import 'package:test_futter_project/presentation/pages/messages/widgets/chat_input_bar/chat_input_button.dart';
+import 'package:test_futter_project/presentation/pages/messages/widgets/chat_input_bar/chat_input_text_field.dart';
+
+import '../../../../../common/app_dimensions.dart';
+
+class ChatInputBar extends StatelessWidget {
+  final TextEditingController messageTextController;
+  final FocusNode messageFocusNode;
+  final VoidCallback? onMessageSent;
+
+  const ChatInputBar({
+    required this.messageTextController,
+    required this.messageFocusNode,
+    this.onMessageSent,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MessagesPageCubit, MessagesPageState>(
+      builder: (context, state) {
+        return Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          spacing: AppDimensions.minorL,
+          children: [
+            ChatInputButton(
+              icon: Icons.attach_file,
+              onTap: () {},
+              iconRotationAngleDegrees: 40,
+              semanticsLabel: AppSemanticsLabels.chatInputBarAttachmentButton,
+            ),
+
+            Expanded(
+              child: ChatInputTextField(
+                focusNode: messageFocusNode,
+                textEditingController: messageTextController,
+                sendMessage: (context, state) => sendMessage(context, state),
+              ),
+            ),
+
+            ChatInputButton(
+              icon: Icons.send,
+              onTap: state.currentMessageText.isEmpty ? null : () => sendMessage(context, state),
+              iconRotationAngleDegrees: -40,
+              semanticsLabel: AppSemanticsLabels.chatInputBarSendMessageButton,
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void sendMessage(BuildContext context, MessagesPageState state) {
+    final user = context.read<UserDataCubit>().user;
+
+    context.read<InboxPageCubit>().sendMessage(
+      state.currentConversationId,
+      MessageModel(user.userId, MessageStatus.sent, messageTextController.text, DateTime.now()),
+    );
+
+    onMessageSent?.call();
+
+    context.read<MessagesPageCubit>().updateMessageText('');
+    messageTextController.clear();
+
+    messageFocusNode.requestFocus();
+  }
+}
