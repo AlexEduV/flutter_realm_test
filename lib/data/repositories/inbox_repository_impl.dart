572b43e63440ea009fe9a7e2d22d197fb533d87d
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-24 15:43:15 +0200

    refactoring;

--- lib/data/repositories/inbox_repository_impl.dart
+++ lib/data/repositories/inbox_repository_impl.dart
@@ -1,9 +1,6 @@
-import 'package:test_futter_project/di/injection_container.dart';
 import 'package:test_futter_project/domain/data_sources/remote/messages_remote_data_source.dart';
-import 'package:test_futter_project/domain/entities/user_entity.dart';
 import 'package:test_futter_project/domain/models/conversation_model.dart';
 import 'package:test_futter_project/domain/repositories/inbox_repository.dart';
-import 'package:test_futter_project/domain/usecases/users/get_user_by_id_use_case.dart';
 
 class InboxRepositoryImpl implements InboxRepository {
   final MessagesRemoteDataSource _messagesRemoteDataSource;
@@ -28,16 +25,5 @@ class InboxRepositoryImpl implements InboxRepository {
   @override
   ConversationModel getConversationByOwnerId(String ownerId) {
     return _messagesRemoteDataSource.getConversationByOwnerId(ownerId);
-  }
-
-  @override
-  Map<String, UserEntity?> extractUsersFromConversation(ConversationModel conversation) {
-    final senderIds = conversation.messages.map((m) => m.senderId).toSet();
-
-    final userMap = <String, UserEntity?>{
-      for (final id in senderIds) id: serviceLocator<GetUserByIdUseCase>().call(id),
-    };
-
-    return userMap;
   }
 }
--- lib/domain/usecases/inbox/extract_users_from_conversation_use_case.dart
+++ lib/domain/usecases/inbox/extract_users_from_conversation_use_case.dart
@@ -1,17 +1,23 @@
 import 'package:test_futter_project/domain/entities/user_entity.dart';
 import 'package:test_futter_project/domain/models/conversation_model.dart';
-import 'package:test_futter_project/domain/repositories/inbox_repository.dart';
+import 'package:test_futter_project/domain/usecases/users/get_user_by_id_use_case.dart';
 
 import '../usecase.dart';
 
 class ExtractUsersFromConversationUseCase
     extends UseCaseWithParams<ConversationModel, Map<String, UserEntity?>> {
-  final InboxRepository inboxRepository;
+  final GetUserByIdUseCase _getUserByIdUseCase;
 
-  ExtractUsersFromConversationUseCase(this.inboxRepository);
+  ExtractUsersFromConversationUseCase(this._getUserByIdUseCase);
 
   @override
   Map<String, UserEntity?> call(ConversationModel conversation) {
-    return inboxRepository.extractUsersFromConversation(conversation);
+    final senderIds = conversation.messages.map((m) => m.senderId).toSet();
+
+    final userMap = <String, UserEntity?>{
+      for (final id in senderIds) id: _getUserByIdUseCase.call(id),
+    };
+
+    return userMap;
   }
 }
