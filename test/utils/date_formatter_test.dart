5fcd22df64423e02830bd4e31db4f0cca9c9a144
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-25 11:50:54 +0200

    covered date formatter;

--- test/utils/date_formatter_test.dart
+++ test/utils/date_formatter_test.dart
@@ -9,7 +9,11 @@ void main() {
   initializeDateFormatting('en');
 
   final appLocalisationsCubit = AppLocalisationsCubit();
-  final localisations = {'app.locale': 'en', 'dateFormatting.yesterday': 'Yesterday'};
+  final localisations = {
+    'app.locale': 'en',
+    'dateFormatting.yesterday': 'Yesterday',
+    'dateFormatting.today': 'Today',
+  };
 
   appLocalisationsCubit.load(localisations);
 
@@ -25,7 +29,6 @@ void main() {
     test('returns time for today\'s date', () {
       final now = DateTime.now();
       final formatted = DateFormatter.formatSmartDate(now);
-      // Should match HH:mm format
       final expected = DateFormat.Hm().format(now);
       expect(formatted, expected);
     });
@@ -37,18 +40,57 @@ void main() {
     });
 
     test('returns weekday for other dates', () {
-      // Pick a date 2 days ago
       final twoDaysAgo = DateTime.now().subtract(const Duration(days: 2));
       final formatted = DateFormatter.formatSmartDate(twoDaysAgo);
-      final expected = DateFormat.E().format(twoDaysAgo);
+      final expected =
+          DateFormat.E('en').format(twoDaysAgo)[0].toUpperCase() +
+          DateFormat.E('en').format(twoDaysAgo).substring(1);
       expect(formatted, expected);
     });
 
     test('returns weekday for a date in the future', () {
       final futureDate = DateTime.now().add(const Duration(days: 3));
       final formatted = DateFormatter.formatSmartDate(futureDate);
-      final expected = DateFormat.E().format(futureDate);
+      final expected =
+          DateFormat.E('en').format(futureDate)[0].toUpperCase() +
+          DateFormat.E('en').format(futureDate).substring(1);
       expect(formatted, expected);
+    });
+
+    test('returns empty string for null date', () {
+      expect(DateFormatter.formatSmartDate(null), '');
+    });
+  });
+
+  group('DateFormatter.formatMessageDividerDate', () {
+    test('returns "Today" for today\'s date', () {
+      final now = DateTime.now();
+      final formatted = DateFormatter.formatMessageDividerDate(now);
+      expect(formatted, 'Today');
+    });
+
+    test('returns "Yesterday" for yesterday\'s date', () {
+      final yesterday = DateTime.now().subtract(const Duration(days: 1));
+      final formatted = DateFormatter.formatMessageDividerDate(yesterday);
+      expect(formatted, 'Yesterday');
+    });
+
+    test('returns "March 16" for a date this year', () {
+      final date = DateTime(DateTime.now().year, 3, 16);
+      final formatted = DateFormatter.formatMessageDividerDate(date);
+      final expected = DateFormat('MMMM d', 'en').format(date);
+      expect(formatted, expected);
+    });
+
+    test('returns "March 16, 2015" for a date in a previous year', () {
+      final date = DateTime(2015, 3, 16);
+      final formatted = DateFormatter.formatMessageDividerDate(date);
+      final expected = DateFormat('MMMM d, y', 'en').format(date);
+      expect(formatted, expected);
+    });
+
+    test('returns empty string for null date', () {
+      expect(DateFormatter.formatMessageDividerDate(null), '');
     });
   });
 }
