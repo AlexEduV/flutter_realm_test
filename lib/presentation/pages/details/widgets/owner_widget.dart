7ac6e2e1e7126bbfd207ee77a0a31f3878b2f83d
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-26 15:17:22 +0200

    fixed owner widget logic, if the owner is the current user;

--- lib/presentation/pages/details/widgets/owner_widget.dart
+++ lib/presentation/pages/details/widgets/owner_widget.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:test_futter_project/common/app_semantics_labels.dart';
 import 'package:test_futter_project/common/extensions/context_extension.dart';
 import 'package:test_futter_project/di/injection_container.dart';
 import 'package:test_futter_project/domain/entities/car_entity.dart';
+import 'package:test_futter_project/domain/entities/owner_entity.dart';
 import 'package:test_futter_project/domain/usecases/inbox/get_conversation_by_owner_id_use_case.dart';
+import 'package:test_futter_project/presentation/bloc/user/user_data_cubit.dart';
 import 'package:test_futter_project/presentation/widgets/app_semantics.dart';
 import 'package:test_futter_project/presentation/widgets/avatar_widget.dart';
 
@@ -22,6 +25,7 @@ class OwnerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final owner = car.owner;
+    final user = context.read<UserDataCubit>().user;
 
     return Container(
       padding: const EdgeInsets.only(top: AppDimensions.normalL),
@@ -93,47 +97,48 @@ class OwnerWidget extends StatelessWidget {
 
           const SizedBox(height: AppDimensions.normalL),
 
-          SizedBox(
-            width: double.infinity, // Makes the button full width
-            child: AppSemantics(
-              button: true,
-              label: AppSemanticsLabels.detailsPageContactButton,
-              child: ElevatedButton(
-                onPressed: () {
-                  final ownerId = owner?.id;
-                  if (ownerId == null) return;
-
-                  final conversationId = serviceLocator<GetConversationByOwnerIdUseCase>()
-                      .call(ownerId)
-                      .conversationId;
-
-                  context.go(
-                    '${AppRoutes.home}${AppRoutes.details}/${AppRoutes.inbox}',
-                    extra: conversationId,
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: AppDimensions.normalM,
-                  ), // Button height
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                      AppDimensions.normalXL,
-                    ), // Optional rounded corners
+          if (owner?.id != null && owner?.id != user.userId) ...[
+            SizedBox(
+              width: double.infinity, // Makes the button full width
+              child: AppSemantics(
+                button: true,
+                label: AppSemanticsLabels.detailsPageContactButton,
+                child: ElevatedButton(
+                  onPressed: () => onSendMessageButtonTap(owner, context),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: AppDimensions.normalM,
+                    ), // Button height
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        AppDimensions.normalXL,
+                      ), // Optional rounded corners
+                    ),
+                    backgroundColor: AppColors.headerColor,
+                    foregroundColor: Colors.white,
                   ),
-                  backgroundColor: AppColors.headerColor,
-                  foregroundColor: Colors.white,
-                ),
-                child: Text(
-                  context.tr(L10nKeys.ownerSectionContactButtonTitle),
-                  style: AppTextStyles.zonaPro16,
-                  textAlign: TextAlign.center,
+                  child: Text(
+                    context.tr(L10nKeys.ownerSectionContactButtonTitle),
+                    style: AppTextStyles.zonaPro16,
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
             ),
-          ),
+          ],
         ],
       ),
     );
+  }
+
+  void onSendMessageButtonTap(OwnerEntity? owner, BuildContext context) {
+    final ownerId = owner?.id;
+    if (ownerId == null) return;
+
+    final conversationId = serviceLocator<GetConversationByOwnerIdUseCase>()
+        .call(ownerId)
+        .conversationId;
+
+    context.go('${AppRoutes.home}${AppRoutes.details}/${AppRoutes.inbox}', extra: conversationId);
   }
 }
