55f68c7d13202777c9f9b45c2ad96cf11da9ba9d
Alexander Aks'onov <m.mirvua@gmail.com>
2026-03-30 11:40:55 +0300

    added a page view arrow bar;

--- lib/presentation/pages/home/new_item_page/new_item_page.dart
+++ lib/presentation/pages/home/new_item_page/new_item_page.dart
@@ -66,197 +66,226 @@ class _NewItemPageState extends State<NewItemPage> {
           vertical: AppDimensions.normalL,
           horizontal: AppDimensions.normalM,
         ),
-        child: PageView(
-          controller: pageViewController,
+        child: Column(
           children: [
-            //page 1
-            Column(
-              children: [
-                const Text(
-                  'What kind of an item would you like to add?',
-                  style: AppTextStyles.zonaPro14,
-                ),
-
-                RadioGroup<CarType>(
-                  groupValue: selectedCarType,
-                  onChanged: (CarType? value) {
-                    setState(() {
-                      selectedCarType = value;
-                    });
-                  },
-                  child: const Column(
+            Expanded(
+              child: PageView(
+                controller: pageViewController,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  //page 1
+                  Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      ListTile(
-                        title: Text('Car'),
-                        leading: Radio<CarType>(toggleable: true, value: CarType.car),
+                    children: [
+                      const Text(
+                        'What kind of an item would you like to add?',
+                        style: AppTextStyles.zonaPro14,
                       ),
-                      ListTile(
-                        title: Text('Bike'),
-                        leading: Radio<CarType>(value: CarType.bike),
-                      ),
-                      ListTile(
-                        title: Text('Truck'),
-                        leading: Radio<CarType>(value: CarType.truck),
+
+                      RadioGroup<CarType>(
+                        groupValue: selectedCarType,
+                        onChanged: (CarType? value) {
+                          setState(() {
+                            selectedCarType = value;
+                          });
+                        },
+                        child: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            ListTile(
+                              title: Text('Car'),
+                              leading: Radio<CarType>(toggleable: true, value: CarType.car),
+                            ),
+                            ListTile(
+                              title: Text('Bike'),
+                              leading: Radio<CarType>(value: CarType.bike),
+                            ),
+                            ListTile(
+                              title: Text('Truck'),
+                              leading: Radio<CarType>(value: CarType.truck),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
-                ),
-              ],
+
+                  //page 2
+                  Column(
+                    children: [
+                      AppFormField(
+                        focusNode: manufacturerFocusNode,
+                        textEditingController: manufacturerTextController,
+                        labelText: 'Manufacturer',
+                        hintText: 'Manufacturer',
+                        textInputType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                        onFocusChange: (hasFocus) {},
+                        onChanged: (newText) {},
+                        padding: 0.0,
+                      ),
+
+                      AppFormField(
+                        focusNode: modelFocusNode,
+                        textEditingController: modelTextController,
+                        labelText: 'Model',
+                        hintText: 'Model',
+                        textInputType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                        onFocusChange: (hasFocus) {},
+                        onChanged: (newText) {},
+                        padding: 0.0,
+                      ),
+
+                      AppFormField(
+                        focusNode: yearFocusNode,
+                        textEditingController: yearTextController,
+                        labelText: 'Year',
+                        hintText: 'Year',
+                        textInputType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                        onFocusChange: (hasFocus) {},
+                        onChanged: (newText) {},
+                        padding: 0.0,
+                      ),
+
+                      AppFormField(
+                        focusNode: colorFocusNode,
+                        textEditingController: colorTextController,
+                        labelText: 'Color',
+                        hintText: 'Color',
+                        textInputType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                        onFocusChange: (hasFocus) {},
+                        onChanged: (newText) {},
+                        padding: 0.0,
+                      ),
+                    ],
+                  ),
+
+                  // //page 3
+                  // const Text('Body type', style: AppTextStyles.zonaPro14),
+                  //
+                  // RadioGroup<BodyType>(
+                  //   groupValue: selectedBodyType,
+                  //   onChanged: (BodyType? value) {
+                  //     setState(() {
+                  //       selectedBodyType = value;
+                  //     });
+                  //   },
+                  //   child: const Column(
+                  //     crossAxisAlignment: CrossAxisAlignment.start,
+                  //     children: <Widget>[
+                  //       ListTile(
+                  //         title: Text('Sedan'),
+                  //         leading: Radio<BodyType>(toggleable: true, value: BodyType.sedan),
+                  //       ),
+                  //       ListTile(
+                  //         title: Text('Coupe'),
+                  //         leading: Radio<BodyType>(value: BodyType.coupe),
+                  //       ),
+                  //       ListTile(
+                  //         title: Text('Minivan'),
+                  //         leading: Radio<BodyType>(value: BodyType.minivan),
+                  //       ),
+                  //       ListTile(
+                  //         title: Text('Hatchback'),
+                  //         leading: Radio<BodyType>(value: BodyType.hatchback),
+                  //       ),
+                  //       ListTile(
+                  //         title: Text('Universal'),
+                  //         leading: Radio<BodyType>(value: BodyType.universal),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                  //
+                  // const Text('Fuel type', style: AppTextStyles.zonaPro14),
+                  //
+                  // RadioGroup<FuelType>(
+                  //   groupValue: selectedFuelType,
+                  //   onChanged: (FuelType? value) {
+                  //     setState(() {
+                  //       selectedFuelType = value;
+                  //     });
+                  //   },
+                  //   child: const Column(
+                  //     crossAxisAlignment: CrossAxisAlignment.start,
+                  //     children: <Widget>[
+                  //       ListTile(
+                  //         title: Text('Diesel'),
+                  //         leading: Radio<FuelType>(toggleable: true, value: FuelType.diesel),
+                  //       ),
+                  //       ListTile(
+                  //         title: Text('Gasoline'),
+                  //         leading: Radio<FuelType>(value: FuelType.gasoline),
+                  //       ),
+                  //       ListTile(
+                  //         title: Text('EV'),
+                  //         leading: Radio<FuelType>(value: FuelType.ev),
+                  //       ),
+                  //       ListTile(
+                  //         title: Text('Hybrid'),
+                  //         leading: Radio<FuelType>(value: FuelType.hybrid),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                  //
+                  // const Text('Transmission type', style: AppTextStyles.zonaPro14),
+                  //
+                  // RadioGroup<TransmissionType>(
+                  //   groupValue: selectedTransmissionType,
+                  //   onChanged: (TransmissionType? value) {
+                  //     setState(() {
+                  //       selectedTransmissionType = value;
+                  //     });
+                  //   },
+                  //   child: const Column(
+                  //     crossAxisAlignment: CrossAxisAlignment.start,
+                  //     children: <Widget>[
+                  //       ListTile(
+                  //         title: Text('Manual'),
+                  //         leading: Radio<TransmissionType>(
+                  //           toggleable: true,
+                  //           value: TransmissionType.manual,
+                  //         ),
+                  //       ),
+                  //       ListTile(
+                  //         title: Text('Automatic'),
+                  //         leading: Radio<TransmissionType>(value: TransmissionType.automatic),
+                  //       ),
+                  //       ListTile(
+                  //         title: Text('Hybrid'),
+                  //         leading: Radio<TransmissionType>(value: TransmissionType.hybrid),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
 
-            //page 2
-            Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: AppDimensions.minorL,
               children: [
-                AppFormField(
-                  focusNode: manufacturerFocusNode,
-                  textEditingController: manufacturerTextController,
-                  labelText: 'Manufacturer',
-                  hintText: 'Manufacturer',
-                  textInputType: TextInputType.text,
-                  textInputAction: TextInputAction.next,
-                  onFocusChange: (hasFocus) {},
-                  onChanged: (newText) {},
-                  padding: 0.0,
+                IconButton(
+                  onPressed: () => pageViewController.previousPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  ),
+                  icon: const Icon(Icons.chevron_left_outlined, color: AppColors.headerColor),
                 ),
-
-                AppFormField(
-                  focusNode: modelFocusNode,
-                  textEditingController: modelTextController,
-                  labelText: 'Model',
-                  hintText: 'Model',
-                  textInputType: TextInputType.text,
-                  textInputAction: TextInputAction.next,
-                  onFocusChange: (hasFocus) {},
-                  onChanged: (newText) {},
-                  padding: 0.0,
-                ),
-
-                AppFormField(
-                  focusNode: yearFocusNode,
-                  textEditingController: yearTextController,
-                  labelText: 'Year',
-                  hintText: 'Year',
-                  textInputType: TextInputType.text,
-                  textInputAction: TextInputAction.next,
-                  onFocusChange: (hasFocus) {},
-                  onChanged: (newText) {},
-                  padding: 0.0,
-                ),
-
-                AppFormField(
-                  focusNode: colorFocusNode,
-                  textEditingController: colorTextController,
-                  labelText: 'Color',
-                  hintText: 'Color',
-                  textInputType: TextInputType.text,
-                  textInputAction: TextInputAction.next,
-                  onFocusChange: (hasFocus) {},
-                  onChanged: (newText) {},
-                  padding: 0.0,
+                IconButton(
+                  onPressed: () => pageViewController.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  ),
+                  icon: const Icon(Icons.chevron_right_outlined, color: AppColors.headerColor),
                 ),
               ],
             ),
-
-            // //page 3
-            // const Text('Body type', style: AppTextStyles.zonaPro14),
-            //
-            // RadioGroup<BodyType>(
-            //   groupValue: selectedBodyType,
-            //   onChanged: (BodyType? value) {
-            //     setState(() {
-            //       selectedBodyType = value;
-            //     });
-            //   },
-            //   child: const Column(
-            //     crossAxisAlignment: CrossAxisAlignment.start,
-            //     children: <Widget>[
-            //       ListTile(
-            //         title: Text('Sedan'),
-            //         leading: Radio<BodyType>(toggleable: true, value: BodyType.sedan),
-            //       ),
-            //       ListTile(
-            //         title: Text('Coupe'),
-            //         leading: Radio<BodyType>(value: BodyType.coupe),
-            //       ),
-            //       ListTile(
-            //         title: Text('Minivan'),
-            //         leading: Radio<BodyType>(value: BodyType.minivan),
-            //       ),
-            //       ListTile(
-            //         title: Text('Hatchback'),
-            //         leading: Radio<BodyType>(value: BodyType.hatchback),
-            //       ),
-            //       ListTile(
-            //         title: Text('Universal'),
-            //         leading: Radio<BodyType>(value: BodyType.universal),
-            //       ),
-            //     ],
-            //   ),
-            // ),
-            //
-            // const Text('Fuel type', style: AppTextStyles.zonaPro14),
-            //
-            // RadioGroup<FuelType>(
-            //   groupValue: selectedFuelType,
-            //   onChanged: (FuelType? value) {
-            //     setState(() {
-            //       selectedFuelType = value;
-            //     });
-            //   },
-            //   child: const Column(
-            //     crossAxisAlignment: CrossAxisAlignment.start,
-            //     children: <Widget>[
-            //       ListTile(
-            //         title: Text('Diesel'),
-            //         leading: Radio<FuelType>(toggleable: true, value: FuelType.diesel),
-            //       ),
-            //       ListTile(
-            //         title: Text('Gasoline'),
-            //         leading: Radio<FuelType>(value: FuelType.gasoline),
-            //       ),
-            //       ListTile(
-            //         title: Text('EV'),
-            //         leading: Radio<FuelType>(value: FuelType.ev),
-            //       ),
-            //       ListTile(
-            //         title: Text('Hybrid'),
-            //         leading: Radio<FuelType>(value: FuelType.hybrid),
-            //       ),
-            //     ],
-            //   ),
-            // ),
-            //
-            // const Text('Transmission type', style: AppTextStyles.zonaPro14),
-            //
-            // RadioGroup<TransmissionType>(
-            //   groupValue: selectedTransmissionType,
-            //   onChanged: (TransmissionType? value) {
-            //     setState(() {
-            //       selectedTransmissionType = value;
-            //     });
-            //   },
-            //   child: const Column(
-            //     crossAxisAlignment: CrossAxisAlignment.start,
-            //     children: <Widget>[
-            //       ListTile(
-            //         title: Text('Manual'),
-            //         leading: Radio<TransmissionType>(
-            //           toggleable: true,
-            //           value: TransmissionType.manual,
-            //         ),
-            //       ),
-            //       ListTile(
-            //         title: Text('Automatic'),
-            //         leading: Radio<TransmissionType>(value: TransmissionType.automatic),
-            //       ),
-            //       ListTile(
-            //         title: Text('Hybrid'),
-            //         leading: Radio<TransmissionType>(value: TransmissionType.hybrid),
-            //       ),
-            //     ],
-            //   ),
-            // ),
           ],
         ),
       ),
