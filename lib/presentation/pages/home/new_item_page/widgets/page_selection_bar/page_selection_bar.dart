668fbe14dcd879a528f17ab857b21895df74bc0d
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-01 14:15:49 +0300

    switched to a dedicated library for page indicator animation;

--- lib/presentation/pages/home/new_item_page/widgets/page_selection_bar/page_selection_bar.dart
+++ lib/presentation/pages/home/new_item_page/widgets/page_selection_bar/page_selection_bar.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:test_futter_project/common/enums/item_setup_tab.dart';
-import 'package:test_futter_project/presentation/pages/home/new_item_page/widgets/page_selection_bar/widgets/page_dot_widget.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../../../../../common/app_colors.dart';
 import '../../../../../../common/app_dimensions.dart';
 
-class PageSelectionBar extends StatefulWidget {
+class PageSelectionBar extends StatelessWidget {
   final Function() onForwardPressed;
   final Function() onBackPressed;
   final int currentIndex;
@@ -22,17 +21,8 @@ class PageSelectionBar extends StatefulWidget {
   });
 
   @override
-  State<PageSelectionBar> createState() => _PageSelectionBarState();
-}
-
-class _PageSelectionBarState extends State<PageSelectionBar> {
-  int? _prevIndex;
-
-  @override
   Widget build(BuildContext context) {
-    final spacingBetweenDots = AppDimensions.minorL;
     final dotSize = AppDimensions.normalS;
-    final isMoving = _prevIndex != null && _prevIndex != widget.currentIndex;
 
     return Container(
       decoration: BoxDecoration(
@@ -54,83 +44,32 @@ class _PageSelectionBarState extends State<PageSelectionBar> {
           Padding(
             padding: const EdgeInsets.all(AppDimensions.minorM),
             child: IconButton(
-              icon: Icon(
-                Icons.chevron_left_outlined,
-                color: widget.iconColor,
-                size: widget.iconSize,
-              ),
-              onPressed: widget.onBackPressed,
+              icon: Icon(Icons.chevron_left_outlined, color: iconColor, size: iconSize),
+              onPressed: onBackPressed,
             ),
           ),
 
-          SizedBox(
-            width:
-                ItemSetupTab.values.length * dotSize +
-                (spacingBetweenDots * (ItemSetupTab.values.length - 1)),
-            child: Stack(
-              alignment: AlignmentGeometry.center,
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  spacing: spacingBetweenDots,
-                  children: [
-                    ...ItemSetupTab.values.map((element) {
-                      return const PageDotWidget();
-                    }),
-                  ],
-                ),
-
-                //the first index is working, but on index change nothing happens
-                AnimatedAlign(
-                  alignment: Alignment(getXAlignment(), 0.0),
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.fastOutSlowIn,
-                  onEnd: () {
-                    setState(() {
-                      _prevIndex = null; // Reset after animation
-                    });
-                  },
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.fastOutSlowIn,
-                    height: dotSize * 1.2,
-                    width: isMoving ? dotSize * 2.2 : dotSize * 1.2,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(16.0)),
-                      color: AppColors.headerColor,
-                    ),
-                  ),
-                ),
-              ],
+          AnimatedSmoothIndicator(
+            activeIndex: currentIndex,
+            count: 3,
+            effect: WormEffect(
+              activeDotColor: AppColors.headerColor,
+              dotColor: AppColors.placeholderColor,
+              dotHeight: dotSize,
+              dotWidth: dotSize,
             ),
           ),
 
+          // end region --------------
           Padding(
             padding: const EdgeInsets.all(AppDimensions.minorM),
             child: IconButton(
-              icon: Icon(
-                Icons.chevron_right_outlined,
-                color: widget.iconColor,
-                size: widget.iconSize,
-              ),
-              onPressed: widget.onForwardPressed,
+              icon: Icon(Icons.chevron_right_outlined, color: iconColor, size: iconSize),
+              onPressed: onForwardPressed,
             ),
           ),
         ],
       ),
     );
-  }
-
-  @override
-  void didUpdateWidget(covariant PageSelectionBar oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.currentIndex != oldWidget.currentIndex) {
-      _prevIndex = oldWidget.currentIndex;
-    }
-  }
-
-  double getXAlignment() {
-    final alignment = -1.0 + (widget.currentIndex * (2.0 / (ItemSetupTab.values.length - 1)));
-    return alignment;
   }
 }
