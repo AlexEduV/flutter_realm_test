14f660a08cc00aaa6d891d20c6914b64ecf79f4c
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-03 15:31:35 +0300

    working on the error widget;

--- lib/presentation/widgets/network_error_widget.dart
+++ lib/presentation/widgets/network_error_widget.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:test_futter_project/common/app_colors.dart';
+import 'package:test_futter_project/common/app_asset_routes.dart';
 import 'package:test_futter_project/common/app_dimensions.dart';
 import 'package:test_futter_project/common/app_text_styles.dart';
-
-import '../../common/app_asset_routes.dart';
 
 class NetworkErrorWidget extends StatelessWidget {
   const NetworkErrorWidget({super.key});
@@ -11,32 +9,38 @@ class NetworkErrorWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(AppDimensions.normalS),
-      child: Column(
-        spacing: AppDimensions.minorL,
-        children: [
-          Expanded(
-            child: Container(
-              height: 150,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fitWidth,
-                  image: AssetImage(AppAssetRoutes.errorImageRoute),
+      padding: const EdgeInsets.only(top: AppDimensions.normalS),
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppAssetRoutes.errorImageRoute),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(AppDimensions.normalS),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: AppDimensions.minorL,
+            children: [
+              Text(
+                'Whoops!',
+                style: AppTextStyles.zonaPro24.copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.grey,
                 ),
               ),
-            ),
+              Text(
+                'It seems the content is not available',
+                style: AppTextStyles.zonaPro20.copyWith(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.orange,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
           ),
-          Text('Whoops!', style: AppTextStyles.zonaPro24.copyWith(fontWeight: FontWeight.w600)),
-          Text(
-            'It seems the content is not available',
-            style: AppTextStyles.zonaPro20.copyWith(
-              color: AppColors.accentColor,
-              fontWeight: FontWeight.w500,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ],
+        ),
       ),
     );
   }
