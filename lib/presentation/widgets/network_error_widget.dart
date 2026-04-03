7f5625774cddd0a236237f9786da555fab9e9eb8
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-03 15:21:20 +0300

    working on the error widget;

--- lib/presentation/widgets/network_error_widget.dart
+++ lib/presentation/widgets/network_error_widget.dart
@@ -3,6 +3,8 @@ import 'package:test_futter_project/common/app_colors.dart';
 import 'package:test_futter_project/common/app_dimensions.dart';
 import 'package:test_futter_project/common/app_text_styles.dart';
 
+import '../../common/app_asset_routes.dart';
+
 class NetworkErrorWidget extends StatelessWidget {
   const NetworkErrorWidget({super.key});
 
@@ -13,6 +15,16 @@ class NetworkErrorWidget extends StatelessWidget {
       child: Column(
         spacing: AppDimensions.minorL,
         children: [
+          Container(
+            height: 150,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fitWidth,
+                image: AssetImage(AppAssetRoutes.errorImageRoute),
+              ),
+            ),
+          ),
           Text('Whoops!', style: AppTextStyles.zonaPro24.copyWith(fontWeight: FontWeight.w600)),
           Text(
             'It seems the content is not available',
