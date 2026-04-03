ac7239faa28fcc2df560098916f2d056d2493e70
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-03 12:12:01 +0300

    now limiting the logs of network with an app constant;

--- lib/core/network/app_http_client_impl.dart
+++ lib/core/network/app_http_client_impl.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:test_futter_project/common/app_constants.dart';
 import 'package:test_futter_project/core/network/app_http_client.dart';
 import 'package:test_futter_project/core/network/server_failure.dart';
 
@@ -17,25 +18,38 @@ class AppHttpClientImpl implements AppHttpClient {
       final response = await client.get(url);
 
       if (response.statusCode == HttpStatus.notFound) {
-        debugPrint('Not Found on GET request at url ${url.path}, 404');
+        if (AppConstants.showNetworkLogs) {
+          debugPrint('Not Found on GET request at url ${url.path}, 404');
+        }
         return const Left(ServerFailure.notFound);
       }
 
       if (response.statusCode == HttpStatus.unauthorized) {
-        debugPrint('Unauthorised on GET request at url ${url.path}, 401');
+        if (AppConstants.showNetworkLogs) {
+          debugPrint('Unauthorised on GET request at url ${url.path}, 401');
+        }
         return const Left(ServerFailure.unauthorized);
       }
 
       if (response.statusCode != HttpStatus.ok) {
-        debugPrint('Error during GET request at url ${url.path}, status: ${response.statusCode}');
+        if (AppConstants.showNetworkLogs) {
+          debugPrint('Error during GET request at url ${url.path}, status: ${response.statusCode}');
+        }
         return const Left(ServerFailure.internalError);
       }
 
       if (response.body.isEmpty) {
-        debugPrint('Empty body on GET request at url ${url.path}, status: ${response.statusCode}');
+        if (AppConstants.showNetworkLogs) {
+          debugPrint(
+            'Empty body on GET request at url ${url.path}, status: ${response.statusCode}',
+          );
+        }
         return const Left(ServerFailure.notAvailable);
       }
 
+      if (AppConstants.showNetworkLogs) {
+        debugPrint('Successful GET request at url ${url.path}, status: ${response.statusCode}');
+      }
       return Right(response.body);
     } catch (e) {
       debugPrint('Error during GET request at url ${url.path}, exception: $e');
