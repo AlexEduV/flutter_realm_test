594db8f7b02f58128945fd97be9377c24341713c
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-06 09:36:12 +0300

    added decoration;

--- lib/common/logger/app_network_logger_impl.dart
+++ lib/common/logger/app_network_logger_impl.dart
@@ -3,10 +3,15 @@ import 'package:test_futter_project/common/constants/app_constants.dart';
 import 'package:test_futter_project/common/logger/base_logger.dart';
 
 class AppNetworkLoggerImpl implements BaseLogger {
+  final separationLine = '-' * 45;
+
   @override
   void log(String message) {
     if (AppConstants.showNetworkLogs) {
-      debugPrint(message);
+      debugPrint(
+        '[Network]: $message\n'
+        '$separationLine',
+      );
     }
   }
 }
