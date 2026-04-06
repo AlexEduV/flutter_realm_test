c0c7db051ee572e7462ca12700271bd2225a15ee
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-06 16:34:26 +0300

    test changes;

--- lib/data/data_sources/remote/mock_users_remote_data_source_impl.dart
+++ lib/data/data_sources/remote/mock_users_remote_data_source_impl.dart
@@ -10,7 +10,7 @@ import '../../../mocks/mock_users.dart';
 class MockUsersRemoteDataSourceImpl implements UsersRemoteDataSource {
   final mockUsersLocalStorageKey = 'mock_users';
 
-  final String inQuotes = "";
+  final String inQuotes = "abc";
 
   @override
   int getMaxUserId() {
