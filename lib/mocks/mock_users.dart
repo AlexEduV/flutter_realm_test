4a87302652ceda3ad2eb2b877db6aa6b9deda65d
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-07 09:27:10 +0300

    made initial demo users public, so that the CI would work;

--- lib/mocks/mock_users.dart
+++ lib/mocks/mock_users.dart
@@ -1,5 +1,20 @@
 import '../domain/entities/user_entity.dart';
 
 class MockUsers {
-  static List<UserEntity> initialUsers = [];
+  static List<UserEntity> initialUsers = [
+    UserEntity.initial(
+      userId: '16',
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'mock@gmail.com',
+      password: 'qwertyUI10!',
+    ),
+    UserEntity.initial(
+      userId: '17',
+      firstName: 'Jane',
+      lastName: 'Doe',
+      email: 'mock1@gmail.com',
+      password: 'qwertyUI10!',
+    ),
+  ];
 }
