ed48ff26be79bd3349e20f9b56dd86b69e27a699
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-08 14:37:00 +0300

    fixed compile errors in tests;

--- test/presentation/bloc/home/explore_page_cubit_test.dart
+++ test/presentation/bloc/home/explore_page_cubit_test.dart
@@ -94,7 +94,7 @@ void main() {
     'removeCarAt removes the car at the given index',
     build: () => cubit,
     seed: () => ExplorePageState(cars: carList),
-    act: (cubit) => cubit.removeCarById(0),
+    act: (cubit) => cubit.removeCarById('1'),
     expect: () => [
       isA<ExplorePageState>()
           .having((s) => s.cars.length, 'cars.length', 1)
