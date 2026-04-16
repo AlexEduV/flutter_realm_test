2a185f2baa7fbe5c7e7f1839a1c41f904281d9ae
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-16 12:42:01 +0300

    added a use case for the date divider;

---
+++ apps/flutter_realm_test/lib/presentation/pages/messages/widgets/date_divider.usecase.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_flutter_project/presentation/pages/messages/widgets/date_divider.dart';
+import 'package:widgetbook/widgetbook.dart';
+
+import '../../../../common/constants/app_colors.dart';
+import '../../../../common/constants/app_dimensions.dart';
+import '../../../bloc/l10n/app_localisations_cubit.dart';
+
+Widget buildMessageDateDividerUseCase(BuildContext context) {
+  final appLocalisationsCubit = AppLocalisationsCubit()..load({});
+
+  return MultiBlocProvider(
+    providers: [BlocProvider<AppLocalisationsCubit>(create: (_) => appLocalisationsCubit)],
+    child: Scaffold(
+      backgroundColor: AppColors.scaffoldColor,
+      body: Padding(
+        padding: const EdgeInsets.all(AppDimensions.minorL),
+        child: Column(
+          spacing: AppDimensions.normalL,
+          children: [
+            DateDivider(
+              text: context.knobs.string(label: 'Divider label', initialValue: 'Some Text'),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
