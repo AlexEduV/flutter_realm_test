61d23417aa3c99ae57179a634787d5023c650404
Alexander Aks'onov <m.mirvua@gmail.com>
2026-04-20 15:55:02 +0300

    working on tests for the search page;

--- apps/flutter_realm_test/test/presentation/pages/search/search_page_test.dart
+++ apps/flutter_realm_test/test/presentation/pages/search/search_page_test.dart
@@ -1,19 +1,28 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mockito/mockito.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:test_flutter_project/common/constants/app_routes.dart';
 import 'package:test_flutter_project/common/enums/drawer_type.dart';
 import 'package:test_flutter_project/core/di/injection_container.dart';
 import 'package:test_flutter_project/domain/entities/car_entity.dart';
 import 'package:test_flutter_project/domain/entities/user_entity.dart';
+import 'package:test_flutter_project/domain/usecases/permissions/check_location_permission_status_use_case.dart';
 import 'package:test_flutter_project/l10n/l10n_keys.dart';
+import 'package:test_flutter_project/presentation/bloc/home/explore_page/explore_page_cubit.dart';
+import 'package:test_flutter_project/presentation/bloc/home/explore_page/explore_page_state.dart';
+import 'package:test_flutter_project/presentation/bloc/home/home_bottom_bar/home_bottom_bar_cubit.dart';
+import 'package:test_flutter_project/presentation/bloc/home/home_bottom_bar/home_bottom_bar_state.dart';
 import 'package:test_flutter_project/presentation/bloc/l10n/app_localisations_cubit.dart';
 import 'package:test_flutter_project/presentation/bloc/l10n/app_localisations_state.dart';
 import 'package:test_flutter_project/presentation/bloc/search/search_page_cubit.dart';
 import 'package:test_flutter_project/presentation/bloc/search/search_page_state.dart';
 import 'package:test_flutter_project/presentation/bloc/user/user_data_cubit.dart';
 import 'package:test_flutter_project/presentation/bloc/user/user_data_state.dart';
+import 'package:test_flutter_project/presentation/pages/home/home_page.dart';
 import 'package:test_flutter_project/presentation/pages/search/search_page.dart';
 import 'package:test_flutter_project/presentation/pages/search/widgets/filters_drawer.dart';
 import 'package:test_flutter_project/presentation/pages/search/widgets/model_filter_drawer.dart';
@@ -23,18 +32,28 @@ import 'package:test_flutter_project/presentation/widgets/segmented_switch.dart'
 
 import '../../../common/extensions/context_extension_test.mocks.dart';
 import '../../../utils/app_router_test.mocks.dart';
+import '../../bloc/user/user_data_cubit_test.mocks.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   final appLocalisationsCubit = AppLocalisationsCubit();
+  final mockCheckLocationPermissionStatusUseCase = MockCheckLocationPermissionStatusUseCase();
 
   setUpAll(() {
     serviceLocator.registerSingleton<AppLocalisationsCubit>(appLocalisationsCubit);
+    serviceLocator.registerSingleton<CheckLocationPermissionStatusUseCase>(
+      mockCheckLocationPermissionStatusUseCase,
+    );
+
+    when(
+      mockCheckLocationPermissionStatusUseCase.call(),
+    ).thenAnswer((_) async => PermissionStatus.granted);
   });
 
   tearDownAll(() {
     serviceLocator.unregister<AppLocalisationsCubit>();
+    serviceLocator.unregister<CheckLocationPermissionStatusUseCase>();
   });
 
   Widget buildTestableWidget({
@@ -44,18 +63,29 @@ void main() {
     SearchPageState? searchState,
     UserDataState? userState,
   }) {
+    final mockHomeBottomBarCubit = MockHomeBottomBarCubit();
+    final mockExplorePageCubit = MockExplorePageCubit();
+
+    when(mockHomeBottomBarCubit.stream).thenAnswer((_) => const Stream.empty());
+    when(mockHomeBottomBarCubit.state).thenAnswer((_) => const HomeBottomBarState());
+
+    when(mockExplorePageCubit.stream).thenAnswer((_) => const Stream.empty());
+    when(mockExplorePageCubit.state).thenReturn(const ExplorePageState());
+
     final router = GoRouter(
+      initialLocation: AppRoutes.home + AppRoutes.newItem,
       routes: [
         GoRoute(
-          path: '/',
-          builder: (context, state) => MultiBlocProvider(
-            providers: [
-              BlocProvider<SearchPageCubit>.value(value: searchCubit),
-              BlocProvider<UserDataCubit>.value(value: userCubit),
-              BlocProvider<AppLocalisationsCubit>.value(value: appLocalisationsCubit),
-            ],
-            child: const Scaffold(body: SearchPage()),
-          ),
+          path: AppRoutes.home,
+          builder: (context, state) => const HomePage(),
+          routes: [
+            GoRoute(
+              path: AppRoutes.newItem,
+              pageBuilder: (context, state) {
+                return const CupertinoPage(child: SearchPage());
+              },
+            ),
+          ],
         ),
       ],
     );
@@ -83,7 +113,16 @@ void main() {
       appLocalisationsCubit.state,
     ).thenReturn(const AppLocalisationsState(localisations: {L10nKeys.searchPageTitle: 'Search'}));
 
-    return MaterialApp.router(routerConfig: router);
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<SearchPageCubit>.value(value: searchCubit),
+        BlocProvider<UserDataCubit>.value(value: userCubit),
+        BlocProvider<AppLocalisationsCubit>.value(value: appLocalisationsCubit),
+        BlocProvider<HomeBottomBarCubit>.value(value: mockHomeBottomBarCubit),
+        BlocProvider<ExplorePageCubit>.value(value: mockExplorePageCubit),
+      ],
+      child: MaterialApp.router(routerConfig: router),
+    );
   }
 
   testWidgets('shows app bar title and back button', (WidgetTester tester) async {
@@ -248,9 +287,7 @@ void main() {
     await tester.tap(find.byIcon(Icons.arrow_back));
     await tester.pumpAndSettle();
 
-    // The page should be popped; in a real app, you would check navigation stack
-    // For test, just ensure no exceptions are thrown
-    //todo: this test is incorrect, since the pop is not called;
-    expect(find.byType(SearchPage), findsOneWidget);
+    expect(find.byType(SearchPage), findsNothing);
+    expect(find.byType(HomePage), findsOneWidget);
   });
 }
