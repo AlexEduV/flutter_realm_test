8f17222f9094c8308ac8332cb778dcf6af25cc0f
Alexander Aks'onov <m.mirvua@gmail.com>
2026-07-08 12:58:07 +0300

    fixed todo in article_remote_data_source_test.dart;

--- apps/flutter_realm_test/test/domain/data_sources/remote/article_remote_data_source_test.dart
+++ apps/flutter_realm_test/test/domain/data_sources/remote/article_remote_data_source_test.dart
@@ -1,51 +1,63 @@
-//todo: outdated tests;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test_flutter_project/domain/entities/article_entity.dart';
+import 'package:test_flutter_project/domain/entities/author_entity.dart';
 
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mockito/mockito.dart';
-// import 'package:test_flutter_project/data/data_sources/remote/mock_article_remote_data_source_impl.dart';
-// import 'package:test_flutter_project/domain/entities/article_entity.dart';
-// import 'package:test_flutter_project/domain/entities/author_entity.dart';
-//
-// import '../../usecases/articles/fetch_articles_use_case_test.mocks.dart';
-//
-// void main() {
-//   TestWidgetsFlutterBinding.ensureInitialized();
-//
-//   late MockArticleRemoteDataSourceImpl mockService;
-//   late MockArticleRepository repository;
-//
-//   setUp(() {
-//     repository = MockArticleRepository();
-//     mockService = MockArticleRemoteDataSourceImpl();
-//   });
-//
-//   test('getAllArticles returns articles from service', () async {
-//     final articles = [
-//       const ArticleEntity(
-//         id: '1',
-//         title: 'A',
-//         summary: '',
-//         paragraphs: [],
-//         author: AuthorEntity(id: '1', fullName: ''),
-//         datePublished: '',
-//       ),
-//       const ArticleEntity(
-//         id: '2',
-//         title: 'B',
-//         summary: '',
-//         paragraphs: [],
-//         author: AuthorEntity(id: '2', fullName: ''),
-//         datePublished: '',
-//       ),
-//     ];
-//
-//     when(mockService.fetchArticles()).thenAnswer((_) async => articles);
-//
-//     final result = await repository.fetchArticles();
-//
-//     expect(result, articles);
-//     verify(mockService.fetchArticles()).called(1);
-//   });
-// }
+import '../../../data/repositories/article_repository_impl_test.mocks.dart';
 
-void main() {}
+void main() {
+  late MockArticleRemoteDataSource mockDataSource;
+
+  setUp(() {
+    mockDataSource = MockArticleRemoteDataSource();
+  });
+
+  group('ArticleRemoteDataSource', () {
+    test('fetchArticles returns a list of ArticleEntity', () async {
+      final articles = [
+        const ArticleEntity(
+          id: '1',
+          title: 'A',
+          summary: '',
+          paragraphs: [],
+          author: AuthorEntity(id: '1', fullName: ''),
+          datePublished: '',
+        ),
+        const ArticleEntity(
+          id: '2',
+          title: 'B',
+          summary: '',
+          paragraphs: [],
+          author: AuthorEntity(id: '2', fullName: ''),
+          datePublished: '',
+        ),
+      ];
+
+      when(mockDataSource.fetchArticles()).thenAnswer((_) async => articles);
+
+      final result = await mockDataSource.fetchArticles();
+
+      expect(result, isA<List<ArticleEntity>>());
+      expect(result.length, 2);
+      expect(result.first.id, '1');
+    });
+
+    test('getArticleById returns an ArticleEntity', () async {
+      const article = ArticleEntity(
+        id: '1',
+        title: 'A',
+        summary: '',
+        paragraphs: [],
+        author: AuthorEntity(id: '1', fullName: ''),
+        datePublished: '',
+      );
+
+      when(mockDataSource.getArticleById('1')).thenAnswer((_) async => article);
+
+      final result = await mockDataSource.getArticleById('1');
+
+      expect(result, isA<ArticleEntity>());
+      expect(result.id, '1');
+    });
+  });
+}
