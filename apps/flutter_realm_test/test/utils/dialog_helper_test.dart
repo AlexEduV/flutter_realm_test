5cc7ea2038f428f74aca35b7048140ce0f6f2d2f
Alexander Aks'onov <m.mirvua@gmail.com>
2026-07-08 12:21:37 +0300

    fixed todo in dialog_helper_test.dart;

--- apps/flutter_realm_test/test/utils/dialog_helper_test.dart
+++ apps/flutter_realm_test/test/utils/dialog_helper_test.dart
@@ -1,9 +1,19 @@
-//todo: the tests are not working;
-
-/*
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test_flutter_project/core/di/injection_container.dart';
 import 'package:test_flutter_project/domain/models/region_ui_model.dart';
+import 'package:test_flutter_project/domain/usecases/car_colors/get_car_color_by_name_use_case.dart';
+import 'package:test_flutter_project/domain/usecases/car_colors/get_car_color_name_from_color_use_case.dart';
+import 'package:test_flutter_project/domain/usecases/car_colors/get_car_colors_use_case.dart';
+import 'package:test_flutter_project/l10n/l10n_keys.dart';
+import 'package:test_flutter_project/presentation/bloc/account/edit_dialog_cubit.dart';
+import 'package:test_flutter_project/presentation/bloc/home/inbox_page/inbox_page_cubit.dart';
+import 'package:test_flutter_project/presentation/bloc/home/inbox_page/inbox_page_state.dart';
+import 'package:test_flutter_project/presentation/bloc/l10n/app_localisations_cubit.dart';
+import 'package:test_flutter_project/presentation/bloc/messages/messages_page_cubit.dart';
+import 'package:test_flutter_project/presentation/bloc/messages/messages_page_state.dart';
 import 'package:test_flutter_project/presentation/widgets/dialogs/color_picker_dialog/color_picker_dialog.dart';
 import 'package:test_flutter_project/presentation/widgets/dialogs/confirmation_dialog.dart';
 import 'package:test_flutter_project/presentation/widgets/dialogs/country_picker_bottom_sheet.dart';
@@ -13,7 +23,28 @@ import 'package:test_flutter_project/presentation/widgets/dialogs/gifs_picker_bo
 import 'package:test_flutter_project/presentation/widgets/dialogs/inbox_item_menu_bottom_sheet.dart';
 import 'package:test_flutter_project/utils/dialog_helper.dart';
 
+import '../presentation/pages/details/widgets/vehicle_specs_widget_test.mocks.dart';
+import '../presentation/pages/messages/messages_page_test.mocks.dart';
+import '../presentation/widgets/dialogs/color_picker_dialog/color_picker_dialog_test.mocks.dart';
+
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  final appLocalisationsCubit = AppLocalisationsCubit();
+
+  setUpAll(() {
+    appLocalisationsCubit.load({
+      L10nKeys.personalDetailsItemPasswordDialogLabel: 'New Password',
+      L10nKeys.personalDetailsItemPasswordDialogSecondLabel: 'Confirm Password',
+      L10nKeys.conversationDialogDeleteItemTitle: 'Delete conversation',
+      L10nKeys.gifsTextFieldHint: 'Search GIFs',
+      L10nKeys.gifsResultsTrendingLabel: 'Trending',
+      L10nKeys.pickColorDialogTitle: 'Pick a color',
+      L10nKeys.cancelLabel: 'Cancel',
+      L10nKeys.confirmLabel: 'Confirm',
+    });
+  });
+
   testWidgets('showConfirmationDialog shows ConfirmationDialog', (tester) async {
     await tester.pumpWidget(
       MaterialApp(
@@ -49,23 +80,26 @@ void main() {
 
   testWidgets('showEditDialog shows EditPersonalInfoDialog', (tester) async {
     await tester.pumpWidget(
-      MaterialApp(
-        home: Builder(
-          builder: (context) {
-            return ElevatedButton(
-              onPressed: () {
-                DialogHelper.showEditDialog(
-                  context,
-                  title: 'Edit',
-                  initialValue: 'init',
-                  confirmButtonTitle: 'Save',
-                  cancelButtonTitle: 'Cancel',
-                  onConfirm: (_) {},
-                );
-              },
-              child: const Text('Open'),
-            );
-          },
+      MultiBlocProvider(
+        providers: [BlocProvider<EditDialogCubit>(create: (_) => EditDialogCubit())],
+        child: MaterialApp(
+          home: Builder(
+            builder: (context) {
+              return ElevatedButton(
+                onPressed: () {
+                  DialogHelper.showEditDialog(
+                    context,
+                    title: 'Edit',
+                    initialValue: 'init',
+                    confirmButtonTitle: 'Save',
+                    cancelButtonTitle: 'Cancel',
+                    onConfirm: (_) {},
+                  );
+                },
+                child: const Text('Open'),
+              );
+            },
+          ),
         ),
       ),
     );
@@ -81,22 +115,28 @@ void main() {
 
   testWidgets('showEditPasswordDialog shows EditPasswordDialog', (tester) async {
     await tester.pumpWidget(
-      MaterialApp(
-        home: Builder(
-          builder: (context) {
-            return ElevatedButton(
-              onPressed: () {
-                DialogHelper.showEditPasswordDialog(
-                  context,
-                  title: 'Password',
-                  confirmButtonTitle: 'OK',
-                  cancelButtonTitle: 'Cancel',
-                  onConfirm: (_) {},
-                );
-              },
-              child: const Text('Open'),
-            );
-          },
+      MultiBlocProvider(
+        providers: [
+          BlocProvider<AppLocalisationsCubit>.value(value: appLocalisationsCubit),
+          BlocProvider<EditDialogCubit>(create: (_) => EditDialogCubit()),
+        ],
+        child: MaterialApp(
+          home: Builder(
+            builder: (context) {
+              return ElevatedButton(
+                onPressed: () {
+                  DialogHelper.showEditPasswordDialog(
+                    context,
+                    title: 'Password',
+                    confirmButtonTitle: 'OK',
+                    cancelButtonTitle: 'Cancel',
+                    onConfirm: (_) {},
+                  );
+                },
+                child: const Text('Open'),
+              );
+            },
+          ),
         ),
       ),
     );
@@ -110,23 +150,18 @@ void main() {
     expect(find.text('Cancel'), findsOneWidget);
   });
 
-  testWidgets('showCountryPicker shows CountryPickerBottomSheet and returns selected', (
-    tester,
-  ) async {
+  testWidgets('showCountryPicker shows CountryPickerBottomSheet and items', (tester) async {
     final items = [
       const RegionUiModel(code: 'US', countryName: 'United States'),
       const RegionUiModel(code: 'IT', countryName: 'Italy'),
     ];
 
-    RegionUiModel? selected;
     await tester.pumpWidget(
       MaterialApp(
         home: Builder(
           builder: (context) {
             return ElevatedButton(
-              onPressed: () async {
-                selected = await DialogHelper.showCountryPicker(context, items, 0);
-              },
+              onPressed: () => DialogHelper.showCountryPicker(context, items, 0),
               child: const Text('Open'),
             );
           },
@@ -138,21 +173,30 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.byType(CountryPickerBottomSheet), findsOneWidget);
-    // Optionally, simulate selection and test the return value
+    expect(find.text('United States'), findsOneWidget);
+    expect(find.text('Italy'), findsOneWidget);
   });
 
   testWidgets('showInboxItemModalBottomSheet shows InboxItemMenuBottomSheet', (tester) async {
+    final mockInboxCubit = MockInboxPageCubit();
+    when(mockInboxCubit.stream).thenAnswer((_) => const Stream.empty());
+    when(mockInboxCubit.state).thenReturn(const InboxPageState());
+
     await tester.pumpWidget(
-      MaterialApp(
-        home: Builder(
-          builder: (context) {
-            return ElevatedButton(
-              onPressed: () {
-                DialogHelper.showInboxItemModalBottomSheet(context, 'conv1');
-              },
-              child: const Text('Open'),
-            );
-          },
+      MultiBlocProvider(
+        providers: [
+          BlocProvider<AppLocalisationsCubit>.value(value: appLocalisationsCubit),
+          BlocProvider<InboxPageCubit>.value(value: mockInboxCubit),
+        ],
+        child: MaterialApp(
+          home: Builder(
+            builder: (context) {
+              return ElevatedButton(
+                onPressed: () => DialogHelper.showInboxItemModalBottomSheet(context, 'conv1'),
+                child: const Text('Open'),
+              );
+            },
+          ),
         ),
       ),
     );
@@ -165,17 +209,25 @@ void main() {
 
   testWidgets('showGifsPickerModalBottomSheet shows GifsPickerBottomSheet', (tester) async {
     final listKey = GlobalKey<AnimatedListState>();
+    final mockMessagesPageCubit = MockMessagesPageCubit();
+    when(mockMessagesPageCubit.stream).thenAnswer((_) => const Stream.empty());
+    when(mockMessagesPageCubit.state).thenReturn(const MessagesPageState());
+
     await tester.pumpWidget(
-      MaterialApp(
-        home: Builder(
-          builder: (context) {
-            return ElevatedButton(
-              onPressed: () {
-                DialogHelper.showGifsPickerModalBottomSheet(context, listKey);
-              },
-              child: const Text('Open'),
-            );
-          },
+      MultiBlocProvider(
+        providers: [
+          BlocProvider<AppLocalisationsCubit>.value(value: appLocalisationsCubit),
+          BlocProvider<MessagesPageCubit>.value(value: mockMessagesPageCubit),
+        ],
+        child: MaterialApp(
+          home: Builder(
+            builder: (context) {
+              return ElevatedButton(
+                onPressed: () => DialogHelper.showGifsPickerModalBottomSheet(context, listKey),
+                child: const Text('Open'),
+              );
+            },
+          ),
         ),
       ),
     );
@@ -186,31 +238,50 @@ void main() {
     expect(find.byType(GifsPickerBottomSheet), findsOneWidget);
   });
 
-  testWidgets('showColorsPickerDialog shows ColorPickerDialog and returns value', (tester) async {
-    String? result;
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Builder(
-          builder: (context) {
-            return ElevatedButton(
-              onPressed: () async {
-                result = await DialogHelper.showColorsPickerDialog(context, 'red');
+  group('showColorsPickerDialog', () {
+    setUp(() {
+      final mockGetCarColorsUseCase = MockGetCarColorsUseCase();
+      final mockGetCarColorByNameUseCase = MockGetCarColorByNameUseCase();
+      final mockGetCarColorNameFromColorUseCase = MockGetCarColorNameFromColorUseCase();
+
+      when(mockGetCarColorsUseCase.call()).thenReturn({'red': Colors.red, 'blue': Colors.blue});
+      when(mockGetCarColorByNameUseCase.call(any)).thenReturn(Colors.red);
+      when(mockGetCarColorNameFromColorUseCase.call(any)).thenReturn('red');
+
+      serviceLocator.registerSingleton<GetCarColorsUseCase>(mockGetCarColorsUseCase);
+      serviceLocator.registerSingleton<GetCarColorByNameUseCase>(mockGetCarColorByNameUseCase);
+      serviceLocator.registerSingleton<GetCarColorNameFromColorUseCase>(
+        mockGetCarColorNameFromColorUseCase,
+      );
+    });
+
+    tearDown(() {
+      serviceLocator.unregister<GetCarColorsUseCase>();
+      serviceLocator.unregister<GetCarColorByNameUseCase>();
+      serviceLocator.unregister<GetCarColorNameFromColorUseCase>();
+    });
+
+    testWidgets('shows ColorPickerDialog', (tester) async {
+      await tester.pumpWidget(
+        MultiBlocProvider(
+          providers: [BlocProvider<AppLocalisationsCubit>.value(value: appLocalisationsCubit)],
+          child: MaterialApp(
+            home: Builder(
+              builder: (context) {
+                return ElevatedButton(
+                  onPressed: () => DialogHelper.showColorsPickerDialog(context, 'red'),
+                  child: const Text('Open'),
+                );
               },
-              child: const Text('Open'),
-            );
-          },
+            ),
+          ),
         ),
-      ),
-    );
+      );
 
-    await tester.tap(find.text('Open'));
-    await tester.pumpAndSettle();
+      await tester.tap(find.text('Open'));
+      await tester.pumpAndSettle();
 
-    expect(find.byType(ColorPickerDialog), findsOneWidget);
-    // Optionally, simulate selection and test the return value
+      expect(find.byType(ColorPickerDialog), findsOneWidget);
+    });
   });
 }
-
- */
-
-void main() {}
