205e664351422ce511d6b61fb78507e9a749a6e4
Alexander Aks'onov <m.mirvua@gmail.com>
2026-07-16 10:12:04 +0300

    fixed widget_list_extension.dart with using 'this';

--- apps/flutter_realm_test/lib/common/extensions/widget_list_extension.dart
+++ apps/flutter_realm_test/lib/common/extensions/widget_list_extension.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart' show Widget, Divider;
 
 extension WidgetListDividers on List<Widget> {
-  List<Widget> _interleaveDividers(List<Widget> items, {Widget? divider}) {
+  List<Widget> _interleaveDividers({Widget? divider}) {
     final List<Widget> result = [];
-    for (int i = 0; i < items.length; i++) {
-      result.add(items[i]);
-      if (i < items.length - 1) {
+    for (int i = 0; i < length; i++) {
+      result.add(this[i]);
+      if (i < length - 1) {
         result.add(divider ?? const Divider(height: 1));
       }
     }
     return result;
   }
 
-  List<Widget> withDividers({Widget? divider}) => _interleaveDividers(this, divider: divider);
+  List<Widget> withDividers({Widget? divider}) => _interleaveDividers(divider: divider);
 }
