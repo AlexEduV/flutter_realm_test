575cfa83a83621e0cda2c9d8736d21156e4c900a
Alexander Aks'onov <m.mirvua@gmail.com>
2026-07-16 10:25:39 +0300

    minor changes;

--- apps/flutter_realm_test/lib/common/extensions/widget_list_extension.dart
+++ apps/flutter_realm_test/lib/common/extensions/widget_list_extension.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart' show Widget, Divider;
 extension WidgetListDividers on List<Widget> {
   List<Widget> _interleaveDividers({Widget? divider}) {
     if (isEmpty) return [];
+
     final separator = divider ?? const Divider(height: 1);
     final result = List<Widget>.filled(length * 2 - 1, separator);
     for (int i = 0; i < length; i++) {
