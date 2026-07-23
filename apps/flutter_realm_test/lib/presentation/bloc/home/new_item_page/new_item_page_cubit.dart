4f47b8d4333d31922d1661bd1da555653428d908
Alexander Aks'onov <m.mirvua@gmail.com>
2026-07-23 09:07:13 +0300

    fixed regex for max year, which would be incorrect on year change;

--- apps/flutter_realm_test/lib/presentation/bloc/home/new_item_page/new_item_page_cubit.dart
+++ apps/flutter_realm_test/lib/presentation/bloc/home/new_item_page/new_item_page_cubit.dart
@@ -75,7 +75,7 @@ class NewItemPageCubit extends Cubit<NewItemPageState> {
               regexErrorMessage: _appLocalisationsCubit.getLocalisationByKey(
                 L10nKeys.fieldParamsYearOfProductionRegexErrorMessage,
               ),
-              regex: r'^(198[0-9]|199[0-9]|200[0-9]|201[0-9]|202[0-6])$',
+              regex: r'^\d{4}$',
             ),
         priceFieldParams:
             FieldParamsModel.withLabel(
@@ -172,6 +172,12 @@ class NewItemPageCubit extends Cubit<NewItemPageState> {
 
     final yearRegex = RegExp(state.yearFieldParams?.regex ?? '');
     if (!yearRegex.hasMatch(year)) {
+      emit(state.copyWith(yearErrorText: state.yearFieldParams?.regexErrorMessage));
+      return false;
+    }
+
+    final yearInt = int.parse(year);
+    if (yearInt < 1980 || yearInt > DateTime.now().year) {
       emit(state.copyWith(yearErrorText: state.yearFieldParams?.regexErrorMessage));
       return false;
     }
